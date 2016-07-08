a6743cec8d85109cf9a7ababb29bf57cad677832
Péter Szilágyi <peterke@gmail.com>
2016-07-08 17:16:19 +0300

    simulators/dao-hard-fork/extradata: validate malicious mining

--- simulators/dao-hard-fork/extradata/simulator.sh
+++ simulators/dao-hard-fork/extradata/simulator.sh
@@ -1,38 +1,74 @@
 #!/bin/bash
 set -e
 
-# Start a new client node
-echo -n "Starting a new client..."
-id=`curl -sf -X POST $HIVE_SIMULATOR/nodes`
-ip=`curl -sf $HIVE_SIMULATOR/nodes/$id`
-echo "Started node $id at $ip"
+# Start two DAO enabled clients, one pro and one contra fork (don't connect them)
+echo -n "Starting DAO enabled clients..."
 
-# Let it mine past the DAO fork block and the consecutive blocks
-curl -sf -X POST --data '{"jsonrpc":"2.0","method":"miner_start","params":[1],"id":0}' $ip:8545
+proID=`curl -sf -X POST $HIVE_SIMULATOR/nodes?HIVE_FORK_DAO_VOTE=1`
+proIP=`curl -sf $HIVE_SIMULATOR/nodes/$proID`
+echo "Started pro-fork node $proID at $proIP"
 
-latest=0
+conID=`curl -sf -X POST $HIVE_SIMULATOR/nodes?HIVE_FORK_DAO_VOTE=0`
+conIP=`curl -sf $HIVE_SIMULATOR/nodes/$conID`
+echo "Started contra-fork node $conID at $conIP"
+
+# Let both nodes mine past the DAO fork blocks by a few
+curl -sf -X POST --data '{"jsonrpc":"2.0","method":"miner_start","params":[1],"id":0}' $proIP:8545
+
+curl -sf -X POST --data '{"jsonrpc":"2.0","method":"miner_setExtra","params":["dao-hard-fork"],"id":0}' $conIP:8545 # Try to impersoante a pro-forker
+curl -sf -X POST --data '{"jsonrpc":"2.0","method":"miner_start","params":[1],"id":0}' $conIP:8545
+
+proLatest=0
 while [ true ]; do
-	latest=`curl -sf -X POST --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' $ip:8545 | jq '.result' | tr -d '"'`
-	latest=$((16#${latest#*x}))
-	if [ "$latest" -ge "$((HIVE_FORK_DAO + 13))" ]; then
+	proLatest=`curl -sf -X POST --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' $proIP:8545 | jq '.result' | tr -d '"'`
+	proLatest=$((16#${proLatest#*x}))
+	if [ "$proLatest" -ge "$((HIVE_FORK_DAO_BLOCK + 13))" ]; then
 		break
 	fi
 	sleep 1
 done
-curl -sf -X POST --data '{"jsonrpc":"2.0","method":"miner_stop","params":[],"id":2}' $ip:8545
+curl -sf -X POST --data '{"jsonrpc":"2.0","method":"miner_stop","params":[],"id":2}' $proIP:8545
 
-# Retrieve all blocks and validate DAOs' extradata ("dao-hard-fork" -> 0x64616f2d686172642d666f726b)
-for block in `seq 0 $latest`; do
-	extra=`curl -sf -X POST --data "{\"jsonrpc\":\"2.0\",\"method\":\"eth_getBlockByNumber\",\"params\":[$block, false],\"id\":3}" $ip:8545 | jq '.result.extraData' | tr -d '"'`
+conLatest=0
+while [ true ]; do
+	conLatest=`curl -sf -X POST --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' $conIP:8545 | jq '.result' | tr -d '"'`
+	conLatest=$((16#${conLatest#*x}))
+	if [ "$conLatest" -ge "$((HIVE_FORK_DAO_BLOCK + 13))" ]; then
+		break
+	fi
+	sleep 1
+done
+curl -sf -X POST --data '{"jsonrpc":"2.0","method":"miner_stop","params":[],"id":2}' $conIP:8545
 
-	if [ "$block" -ge $HIVE_FORK_DAO ] && [ "$block" -lt $((HIVE_FORK_DAO + 10)) ]; then
+# Retrieve all pro-fork blocks and validate DAO fork range extradata ("dao-hard-fork" -> 0x64616f2d686172642d666f726b)
+for block in `seq 1 $proLatest`; do
+	extra=`curl -sf -X POST --data "{\"jsonrpc\":\"2.0\",\"method\":\"eth_getBlockByNumber\",\"params\":[$block, false],\"id\":3}" $proIP:8545 | jq '.result.extraData' | tr -d '"'`
+
+	if [ "$block" -ge $HIVE_FORK_DAO_BLOCK ] && [ "$block" -lt $((HIVE_FORK_DAO_BLOCK + 10)) ]; then
 		if [ "$extra" != "0x64616f2d686172642d666f726b" ]; then
-			echo "DAO fork start (block #$block) extra-data mismatch: have $extra, want 0x64616f2d686172642d666f726b"
+			echo "[pro] DAO fork start (block #$block) extra-data mismatch: have $extra, want 0x64616f2d686172642d666f726b"
 			exit -1
 		fi
 	else
 		if [ "$extra" == "0x64616f2d686172642d666f726b" ]; then
-			echo "Non DAO fork start (block #$block) has DAO extra-data"
+			echo "[pro] Non DAO fork start (block #$block) has DAO extra-data"
+			exit -1
+		fi
+	fi
+done
+
+# Retrieve all contra-fork blocks and validate DAO fork range extradata (NOT "dao-hard-fork" -> 0x64616f2d686172642d666f726b)
+for block in `seq 1 $conLatest`; do
+	extra=`curl -sf -X POST --data "{\"jsonrpc\":\"2.0\",\"method\":\"eth_getBlockByNumber\",\"params\":[$block, false],\"id\":3}" $conIP:8545 | jq '.result.extraData' | tr -d '"'`
+
+	if [ "$block" -ge $HIVE_FORK_DAO_BLOCK ] && [ "$block" -lt $((HIVE_FORK_DAO_BLOCK + 10)) ]; then
+		if [ "$extra" == "0x64616f2d686172642d666f726b" ]; then
+			echo "[con] DAO fork start (block #$block) has DAO extra-data"
+			exit -1
+		fi
+	else
+		if [ "$extra" != "0x64616f2d686172642d666f726b" ]; then
+			echo "[con] Non DAO fork start (block #$block) extra-data mismatch: have $extra, want 0x64616f2d686172642d666f726b"
 			exit -1
 		fi
 	fi
