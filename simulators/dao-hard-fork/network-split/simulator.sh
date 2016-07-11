42ba0b90b761345af6c4a645c81fa420f72a3b90
Péter Szilágyi <peterke@gmail.com>
2016-07-11 17:14:16 +0300

    simulators/dao-hard-fork/network-split: minor polishes

--- simulators/dao-hard-fork/network-split/simulator.sh
+++ simulators/dao-hard-fork/network-split/simulator.sh
@@ -16,6 +16,18 @@ PRO_FORK_NODES=3  # Number of pro-fork nodes to boot up
 PRE_FORK_PEERS=5  # Number of peers to require pre-fork (total - 1), RPC reply
 POST_FORK_PEERS=2 # Number of peers to require post-fork (same camp - 1), RPC reply
 
+# netPeerCount executes an RPC request to a node to retrieve its current number
+# of connected peers.
+#
+# If looks up the IP address of the node from the hive simulator and executes the
+# actual peer lookup. As the result is hex encoded, it will decode and return it.
+function netPeerCount {
+	local ip=`curl -sf $HIVE_SIMULATOR/nodes/$1`
+	local peers=`curl -sf -X POST --data '{"jsonrpc":"2.0","method":"net_peerCount","params":[],"id":0}' $ip:8545 | jq '.result' | tr -d '"'`
+
+	echo $((16#${peers#*x}))
+}
+
 # Start a batch of clients for the no-fork camp
 nofork=()
 for i in `seq 1 $NO_FORK_NODES`; do
@@ -39,12 +51,9 @@ sleep 3
 
 # Check that we have a full graph for all nodes
 for id in ${allnodes[@]}; do
-	ip=`curl -sf $HIVE_SIMULATOR/nodes/$id`
-	peers=`curl -sf -X POST --data '{"jsonrpc":"2.0","method":"net_peerCount","params":[],"id":0}' $ip:8545 | jq '.result' | tr -d '"'`
-
-	peers=$((16#${peers#*x}))
+	peers=`netPeerCount $id`
 	if [ "$peers" != "$PRE_FORK_PEERS" ]; then
-		echo "Invalid peer count for $ip: have $peers, want $PRE_FORK_PEERS"
+		echo "Invalid peer count for $id: have $peers, want $PRE_FORK_PEERS"
 		exit -1
 	fi
 done
@@ -76,14 +85,24 @@ while [ true ]; do
 done
 curl -sf -X POST --data '{"jsonrpc":"2.0","method":"miner_stop","params":[],"id":3}' $proforkMiner:8545
 
-# Check that we have two disjoint est of nodes
+# Check that we have two disjoint set of nodes
 for id in ${allnodes[@]}; do
-	ip=`curl -sf $HIVE_SIMULATOR/nodes/$id`
-	peers=`curl -sf -X POST --data '{"jsonrpc":"2.0","method":"net_peerCount","params":[],"id":4}' $ip:8545 | jq '.result' | tr -d '"'`
+	# devp2p is a bit feisty, so retry a few times to ensure pending handshakes don't count
+	for i in `seq 1 3`; do
+		# Fetch the current peer count and stop if it's correct
+		peers=`netPeerCount $id`
+		if [ "$peers" == "$POST_FORK_PEERS" ]; then
+			break
+		fi
+		ip=`curl -sf $HIVE_SIMULATOR/nodes/$id`
+		curl -sf -X POST --data '{"jsonrpc":"2.0","method":"admin_peers","params":[],"id":0}' $ip:8545
 
-	peers=$((16#${peers#*x}))
-	if [ "$peers" != "$POST_FORK_PEERS" ]; then
-		echo "Invalid peer count for $ip: have $peers, want $POST_FORK_PEERS"
-		exit -1
-	fi
+		# Seems peer count is wrong, unless too many trials, sleep a bit and retry
+		if [ "$i" == "3" ]; then
+			echo "Invalid peer count for $id: have $peers, want $POST_FORK_PEERS"
+			exit -1
+		fi
+
+		sleep 0.5
+	done
 done
