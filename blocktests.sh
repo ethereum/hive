e941ecde079f0bfa9e1df298104b50dd95d65247
Martin Holst Swende <martin@swende.se>
2017-01-31 14:03:52 +0100

    Fix path in tar

--- blocktests.sh
+++ blocktests.sh
@@ -3,7 +3,7 @@
 hive -sim blocktest -test none -client .*:master  -loglevel 6 > data.json
 curl -o tmp.zip -L https://github.com/holiman/testreport_template/archive/master.zip && unzip tmp.zip && rm tmp.zip
 
-REPORTDIR="`pwd`/testreport"
+REPORTDIR="testreport"
 NOW=$(date +"%Y-%d-%m_%H-%M-%S")
 
 mv ./testreport_template-master/ $REPORTDIR
@@ -20,6 +20,6 @@ echo ");" >> data.jsonp
 mv  data.jsonp $REPORTDIR/
 mv  data.json $REPORTDIR/
 
-tar -cvzf $NOW.tar.gz -C $REPORTDIR . # && rm -rf $REPORTDIR
+tar -cvzf $NOW.tar.gz $REPORTDIR/ # && rm -rf $REPORTDIR
  
 echo "Report generated into $NOW.tar.gz"
