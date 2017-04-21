726a5320f203e107906b198c3892218a534c466b
Martin Holst Swende <martin@swende.se>
2017-04-21 13:47:42 +0200

    Fixed geth's new rigid hex-requirements in genesis

--- simulators/ethereum/consensus/testmodel.py
+++ simulators/ethereum/consensus/testmodel.py
@@ -55,6 +55,13 @@ class Testfile(object):
     def __str__(self):
         return self.filename
 
+def padHash(data):
+
+    if(data[:2] == '0x'):
+        data = data[2:]
+
+    return "0x"+data.zfill(64)
+
 class Testcase(object):
 
     def __init__(self,name, jsondata, testfile):
@@ -122,31 +129,41 @@ class Testcase(object):
     def genesis(self, key = None):
         """ Returns the 'genesis' block for this testcase,
         including any alloc's (prestate) required """
+
+
         # Genesis block
         if self.raw_genesis is None:
+
+            #We must fix some fields in the genesis, geth is picky about that
+            fields_to_fix = ['nonce','coinbase','hash','mixHash','parentHash','receiptTrie','stateRoot','transactionsTrie','uncleHash']
+        
+
             raw_genesis = self.data['genesisBlockHeader']
 
-            # Turns out the testcases have noncewritten as 0102030405060708.
-            # Which is supposed to be interpreted as 0x0102030405060708.
-            # But if it's written as 0102030405060708 in the genesis file,
-            # it's interpreted differently. So we'll need to mod that on the fly
-            # for every testcase.
-            if 'nonce' in raw_genesis:
-                nonce = raw_genesis[u'nonce']
-                if not raw_genesis[u'nonce'][:2] == '0x':
-                    raw_genesis[u'nonce'] = '0x'+raw_genesis[u'nonce']
+            for key in fields_to_fix:
+                v = raw_genesis[key]
+                if not v[:2] == '0x':
+                    raw_genesis[key] = '0x'+raw_genesis[key]
 
-            # Also, testcases have 'code' written as 0xdead
-            # But geth does not handle that, so we'll need to mod any of those also
-            # However, cpp-ethereum rejects 'code' written as 'dead'
-#            for addr, account in self.data['pre'].items():
-#                if 'code' in account:
-#                    code = account['code']
-#                    if code[:2] == '0x':
-#                        account['code'] = code[2:]
+            # And fix the alloc-section
+            alloc = {}
+
+            for addr in self.data['pre']:
+                v = self.data['pre'][addr]
+                if not addr[:2] == '0x':
+                    addr = '0x'+addr
+                
+                if 'storage' in v.keys():
+                    storage = {}
+                    for slot,data in v['storage'].items():
+                        _slot = padHash(slot)
+                        _data = padHash(data)
+                        storage[_slot] = _data
+                    v['storage'] = storage
+                alloc[addr] = v
 
 
-            raw_genesis['alloc'] = self.data['pre']
+            raw_genesis['alloc'] = alloc
             self.raw_genesis = raw_genesis
 
         if key is None:
