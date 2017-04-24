5045d59a2ca3e12088bed6b3bd8f8635243e69b3
Martin Holst Swende <martin@swende.se>
2017-04-24 12:23:58 +0200

    Fix bash error in parity scripts

--- clients/parity:stable/parity.sh
+++ clients/parity:stable/parity.sh
@@ -76,7 +76,7 @@ if [ "$HIVE_FORK_SPURIOUS" != "" ]; then
 	chainconfig=`echo $chainconfig | jq "setpath([\"engine\", \"Ethash\", \"params\", \"eip161abcTransition\"]; \"0x$HIVE_FORK_SPURIOUS\")"`
 	chainconfig=`echo $chainconfig | jq "setpath([\"engine\", \"Ethash\", \"params\", \"eip161dTransition\"]; \"0x$HIVE_FORK_SPURIOUS\")"`
 fi
-if [ "$HIVE_FORK_METROPOLIS" != ""]; then
+if [ "$HIVE_FORK_METROPOLIS" != "" ]; then
 	HIVE_FORK_METROPOLIS=`echo "obase=16; $HIVE_FORK_SPURIOUS" | bc`
 	chainconfig=`echo $chainconfig | jq "setpath([\"params\", \"eip98Transition\"]; \"0x$HIVE_FORK_METROPOLIS\")"`
 	chainconfig=`echo $chainconfig | jq "setpath([\"params\", \"eip86Transition\"]; \"0x$HIVE_FORK_METROPOLIS\")"`
