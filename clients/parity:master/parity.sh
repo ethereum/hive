5c191076aaf34945570303cfdf213b5111c04e85
Martin Holst Swende <martin@swende.se>
2017-09-12 08:49:45 +0200

    Added ethash params to parity config

--- clients/parity:master/parity.sh
+++ clients/parity:master/parity.sh
@@ -98,11 +98,18 @@ if [ "$HIVE_FORK_METROPOLIS" != "" ]; then
 
 	HIVE_FORK_METROPOLIS=`echo "obase=16; $HIVE_FORK_METROPOLIS" | bc`
 #	chainconfig=`echo $chainconfig | jq "setpath([\"params\", \"eip98Transition\"]; \"0x$HIVE_FORK_METROPOLIS\")"`
+
+	# Ethash params
+	chainconfig=`echo $chainconfig | jq "setpath([\"engine\", \"Ethash\", \"params\", \"eip649Reward\"]; \"0x29A2241AF62C0000\")"`
+	chainconfig=`echo $chainconfig | jq "setpath([\"engine\", \"Ethash\", \"params\", \"eip649Transition\"]; \"0x$HIVE_FORK_METROPOLIS\")"`
+
+	# General params
     chainconfig=`echo $chainconfig | jq "setpath([\"params\", \"eip140Transition\"]; \"0x$HIVE_FORK_METROPOLIS\")"`
     chainconfig=`echo $chainconfig | jq "setpath([\"params\", \"eip210Transition\"]; \"0x$HIVE_FORK_METROPOLIS\")"`
     chainconfig=`echo $chainconfig | jq "setpath([\"params\", \"eip211Transition\"]; \"0x$HIVE_FORK_METROPOLIS\")"`
     chainconfig=`echo $chainconfig | jq "setpath([\"params\", \"eip214Transition\"]; \"0x$HIVE_FORK_METROPOLIS\")"`
 	chainconfig=`echo $chainconfig | jq "setpath([\"params\", \"eip658Transition\"]; \"0x$HIVE_FORK_METROPOLIS\")"`
+
 
 	# Also new precompiles
 	chainconfig=`echo $chainconfig | jq "setpath([\"accounts\", \"0000000000000000000000000000000000000005\"]; { \"builtin\": { \"name\": \"modexp\", \"activate_at\": \"0x$HIVE_FORK_METROPOLIS\", \"pricing\": { \"modexp\": { \"divisor\": 20 } } } })"`
