e7ba66b173c75037f5fef5a6dc2edd6875ccfd6c
Péter Szilágyi <peterke@gmail.com>
2018-03-12 15:54:50 +0200

    clients: configure ethereumj for byzantium too

--- clients/ethereumj:master/harmony.sh
+++ clients/ethereumj:master/harmony.sh
@@ -16,6 +16,9 @@
 #  - HIVE_FORK_HOMESTEAD block number of the DAO hard-fork transition
 #  - HIVE_FORK_DAO_BLOCK block number of the DAO hard-fork transition
 #  - HIVE_FORK_DAO_VOTE  whether the node support (or opposes) the DAO fork
+#  - HIVE_FORK_TANGERINE block number of TangerineWhistle
+#  - HIVE_FORK_SPURIOUS  block number of SpuriousDragon
+#  - HIVE_FORK_METROPOLIS block number for Metropolis transition
 #  - HIVE_MINER          address to credit with mining rewards (single thread)
 #  - HIVE_MINER_EXTRA    extra-data field to set for newly minted blocks
 
@@ -67,7 +70,9 @@ if [ "$HIVE_FORK_SPURIOUS" != "" ]; then
 	chainconfig=`echo $chainconfig | jq ". + {\"eip158Block\": $HIVE_FORK_SPURIOUS}"`
 	chainconfig=`echo $chainconfig | jq ". + {\"eip155Block\": $HIVE_FORK_SPURIOUS}"`
 fi
-
+if [ "$HIVE_FORK_METROPOLIS" != "" ]; then
+	chainconfig=`echo $chainconfig | jq ". + {\"byzantiumBlock\": $HIVE_FORK_METROPOLIS}"`
+fi
 if [ "$chainconfig" != "{}" ]; then
 	genesis=`cat /genesis.json` && echo $genesis | jq ". + {\"config\": $chainconfig}" > /genesis.json
 fi
