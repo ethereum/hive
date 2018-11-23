a26509771a2a7168a647216c92c7ac9e30446094
Martin Holst Swende <martin@swende.se>
2018-11-23 11:36:53 +0100

    Update clients/go-ethereum_master/geth.sh
    Co-Authored-By: FrankSzendzielarz <33515470+FrankSzendzielarz@users.noreply.github.com>

--- clients/go-ethereum_master/geth.sh
+++ clients/go-ethereum_master/geth.sh
@@ -124,4 +124,4 @@ fi
 # Run the go-ethereum implementation with the requested flags
 echo "Running go-ethereum..."
 echo "$FLAGS"
-/geth $FLAGS  --verbosity=6 --nat=none --rpc --rpcaddr "0.0.0.0" --rpcapi "admin,debug,eth,miner,net,personal,shh,txpool,web3" --ws --wsaddr "0.0.0.0" --wsapi "admin,debug,eth,miner,net,personal,shh,txpool,web3" --wsorigins "*"
+/geth $FLAGS  --verbosity=3 --nat=none --rpc --rpcaddr "0.0.0.0" --rpcapi "admin,debug,eth,miner,net,personal,shh,txpool,web3" --ws --wsaddr "0.0.0.0" --wsapi "admin,debug,eth,miner,net,personal,shh,txpool,web3" --wsorigins "*"
