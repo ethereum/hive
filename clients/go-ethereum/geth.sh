423010c4335506acbe72fd3302188dbdff3f8108
Frank Szendzielarz <Frank@franks.technology>
2019-10-28 18:18:43 +0100

    Graphql for geth tests

--- clients/go-ethereum/geth.sh
+++ clients/go-ethereum/geth.sh
@@ -156,5 +156,5 @@ fi
 # Run the go-ethereum implementation with the requested flags
 
 echo "Running go-ethereum with flags $FLAGS"
-/geth $FLAGS  --verbosity=$HIVE_LOGLEVEL --nat=none --rpc --rpcaddr "0.0.0.0" --rpcapi "admin,debug,eth,miner,net,personal,shh,txpool,web3" --ws --wsaddr "0.0.0.0" --wsapi "admin,debug,eth,miner,net,personal,shh,txpool,web3" --wsorigins "*"
+/geth $FLAGS  --verbosity=$HIVE_LOGLEVEL --nat=none --rpc --rpcaddr "0.0.0.0"  --graphql --graphql.addr "0.0.0.0" --rpcapi "admin,debug,eth,miner,net,personal,shh,txpool,web3" --ws --wsaddr "0.0.0.0" --wsapi "admin,debug,eth,miner,net,personal,shh,txpool,web3" --wsorigins "*"
 
