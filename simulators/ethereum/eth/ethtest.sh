5d3c1a0ccd36b2815de690013ab9f6b0d9356682
renaynay <41963722+renaynay@users.noreply.github.com>
2020-09-22 17:16:52 +0200

    init - do with hive, missing dockerfile

--- simulators/ethereum/eth/ethtest.sh
+++ simulators/ethereum/eth/ethtest.sh
@@ -6,21 +6,17 @@ if test -f enode.json; then
 	rm enode.json
 fi
 
-if test -f geth.log; then
-	rm geth.log
-fi
-
-
 # start configured geth node
-sudo add-apt-repository -y ppa:ethereum/ethereum
-sudo apt-get update
-sudo apt-get install ethereum
+id=`curl -sf -X POST $HIVE_SIMULATOR/nodes`
+echo $id
+
+ip=`curl -sf $HIVE_SIMULATOR/nodes/$id`
 
 /usr/bin/geth --datadir=testchain-data/ --verbosity=5 --nat=none --nodiscover --nousb --http --http.port 7474 --http.api admin > geth.log 2>&1 &
 
 # run the test suite against the node
 sleep 2
-curl -H "Content-type:application/json" -XPOST -d '{"jsonrpc":"2.0","method":"admin_nodeInfo","params":[],"id":67}' http://127.0.0.1:7474 >> enode.json
+curl -H "Content-type:application/json" -XPOST -d '{"jsonrpc":"2.0","method":"admin_nodeInfo","params":[],"id":67}' http://$ip:7474 >> enode.json
 
 # parse enode
 go build parseJson.go
@@ -28,6 +24,8 @@ ENODE=$(go run parseJson.go)
 
 
 ./devp2p rlpx eth-test ${ENODE} chain.rlp.gz genesis.json
+
+curl -sf -X DELETE $HIVE_SIMULATOR/nodes/$id
 
 trap 'pkill geth' EXIT
 
