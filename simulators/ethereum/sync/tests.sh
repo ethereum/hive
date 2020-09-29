1f9bf56e98c274ad4f2835d08e031db27f768b0c
renaynay <41963722+renaynay@users.noreply.github.com>
2020-09-29 12:54:43 +0200

    remove sim provider stuff from sync script

--- simulators/ethereum/sync/tests.sh
+++ simulators/ethereum/sync/tests.sh
@@ -13,7 +13,7 @@ echo "{ \"hostURI\":\"$HIVE_SIMULATOR\" }" >hiveProviderConfig.json
 if [ "${HIVE_DEBUG}" = true ]; then
     dlv test  	--headless  --listen=:2345 --log=true  --api-version=2 -providerConfig="hiveProviderConfig.json"  -test.parallel "$HIVE_PARALLELISM" -test.v
 else
-    go test  -simProvider="hive" -providerConfig="hiveProviderConfig.json"  -test.parallel $HIVE_PARALLELISM  -test.v -test.timeout 0
+    go test -test.parallel $HIVE_PARALLELISM  -test.v -test.timeout 0
 fi
 
 
