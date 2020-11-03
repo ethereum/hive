dfb64fd5cd604d80e355d5674a8eca86b4728d98
Felix Lange <fjl@twurst.com>
2020-11-03 22:44:21 +0100

    simulators/smoke/genesis: new basic lifecycle test (#371)
    * simulators/smoke/genesis: new basic lifecycle test
    
    This re-implements the basic lifecycle test using the new hivesim package.
    
    * clients/openethereum: add defaults for ethash genesis fields
    
    These are optional with other clients.
    
    * clients/trinity: add some defaults for genesis fields

---
+++ simulators/smoke/genesis/go.mod
@@ -0,0 +1,5 @@
+module github.com/ethereum/hive/simulators/smoke/genesis
+
+go 1.13
+
+require github.com/ethereum/hive v0.0.0-20201103195104-fe53923952fc
---
+++ simulators/smoke/genesis/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"github.com/ethereum/hive/hivesim"
+)
+
+func main() {
+	suite := hivesim.Suite{
+		Name:        "genesis",
+		Description: "This test suite checks client initialization with genesis blocks.",
+	}
+	suite.Add(hivesim.ClientTestSpec{
+		Name:        "empty genesis",
+		Description: "This imports an empty genesis block with no environment variables.",
+		Files: map[string]string{
+			"/genesis.json": "genesis-empty.json",
+		},
+		Run: genesisTest{"0x433d0b859a77a29753d2a6df477c971dcc6300af33f9d64d821a1d490b4148b1"}.test,
+	})
+	suite.Add(hivesim.ClientTestSpec{
+		Name:        "all forks",
+		Description: "This imports an empty genesis block and sets all fork block numbers.",
+		Files: map[string]string{
+			"/genesis.json": "genesis-empty.json",
+		},
+		Parameters: map[string]string{
+			"HIVE_CHAIN_ID":            "10",
+			"HIVE_FORK_HOMESTEAD":      "11",
+			"HIVE_FORK_DAO_BLOCK":      "12",
+			"HIVE_FORK_TANGERINE":      "13",
+			"HIVE_FORK_SPURIOUS":       "14",
+			"HIVE_FORK_BYZANTIUM":      "15",
+			"HIVE_FORK_CONSTANTINOPLE": "16",
+			"HIVE_FORK_PETERSBURG":     "17",
+			"HIVE_FORK_ISTANBUL":       "18",
+			"HIVE_FORK_MUIR_GLACIER":   "19",
+			"HIVE_FORK_BERLIN":         "20",
+		},
+		Run: genesisTest{"0x433d0b859a77a29753d2a6df477c971dcc6300af33f9d64d821a1d490b4148b1"}.test,
+	})
+	suite.Add(hivesim.ClientTestSpec{
+		Name:        "non-empty",
+		Description: "This imports a non-empty genesis block.",
+		Files: map[string]string{
+			"/genesis.json": "genesis-nonempty.json",
+		},
+		Parameters: map[string]string{
+			"HIVE_CHAIN_ID":            "10",
+			"HIVE_FORK_HOMESTEAD":      "0",
+			"HIVE_FORK_TANGERINE":      "0",
+			"HIVE_FORK_SPURIOUS":       "0",
+			"HIVE_FORK_BYZANTIUM":      "0",
+			"HIVE_FORK_CONSTANTINOPLE": "0",
+			"HIVE_FORK_PETERSBURG":     "0",
+			"HIVE_FORK_ISTANBUL":       "0",
+		},
+		Run: genesisTest{"0x5ae31c6522bd5856129f66be3d582b842e4e9faaa87f21cce547128339a9db3c"}.test,
+	})
+	hivesim.MustRunSuite(hivesim.New(), suite)
+}
+
+type block struct {
+	Hash string `json:"hash"`
+}
+
+type genesisTest struct {
+	wantHash string
+}
+
+func (g genesisTest) test(t *hivesim.T, c *hivesim.Client) {
+	var b block
+	if err := c.RPC().Call(&b, "eth_getBlockByNumber", "0x0", false); err != nil {
+		t.Fatal("eth_getBlockByNumber call failed:", err)
+	}
+	t.Log("genesis hash", b.Hash)
+	if b.Hash != g.wantHash {
+		t.Fatal("wrong genesis hash, want", g.wantHash)
+	}
+}
