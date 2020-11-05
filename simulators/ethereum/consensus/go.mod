37f56bdd808e33dfb031ce2592d86e34834de0b4
Felix Lange <fjl@twurst.com>
2020-11-05 16:17:20 +0000

    simulators/ethereum/consensus: use package hivesim
    This makes the consensus tests use the new 'hivesim' simulation API
    client. The simulation is now a stand-alone module and no longer
    requires the --sim.rootcontext option. While here, I also fixed some
    staticcheck warnings in the simulation code.
    
    Note that the simulation will behave slightly differently from before
    when run with multiple client types: before this change, a separate
    suite was started for every client. With the new version, the simulation
    will only create a single suite and put all client test results into it.
    
    This behavior change will not be noticed on the production Hive instance
    though, be cause we invoke the simulation separately for every client.
    
    I've tested this using the following command, and it does work as expected:
    
      ./hive --sim consensus --client go-ethereum --docker-noshell --sim.parallelism 8 --sim.testlimit 50

---
+++ simulators/ethereum/consensus/go.mod
@@ -0,0 +1,8 @@
+module github.com/ethereum/hive/simulators/ethereum/consensus2
+
+go 1.13
+
+require (
+	github.com/ethereum/go-ethereum v1.9.23
+	github.com/ethereum/hive v0.0.0-20201105104159-83e7ce57ad38
+)
--- simulators/ethereum/consensus/main.go
+++ simulators/ethereum/consensus/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bytes"
-	"context"
 	"crypto/sha1"
 	"encoding/json"
 	"fmt"
@@ -15,13 +14,11 @@ import (
 	"sync"
 	"time"
 
-	common2 "github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/core"
-	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/rpc"
-	"github.com/ethereum/hive/simulators/common"
-	"github.com/ethereum/hive/simulators/common/providers/hive"
+	"github.com/ethereum/hive/hivesim"
 )
 
 type envvars map[string]int
@@ -216,66 +213,140 @@ var ruleset = map[string]envvars{
 	},
 }
 
-func deliverTests(root string, limit int) chan *testcase {
-	out := make(chan *testcase)
-	var i, j = 0, 0
-	go func() {
-		filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
-			if limit >= 0 && i >= limit {
-				return nil
-			}
-			if info.IsDir() {
-				return nil
-			}
-			if fname := info.Name(); !strings.HasSuffix(fname, ".json") {
-				return nil
-			}
-			tests := make(map[string]BlockTest)
-			data, err := ioutil.ReadFile(path)
-			if err = json.Unmarshal(data, &tests); err != nil {
-				fmt.Println("ERROR:" + path)
-				fmt.Println(err)
-				log.Error("error", "err", err)
-				//return err
-			} else {
-				j = j + 1
-				for name, blocktest := range tests {
-					// t is declared explicitly here, if implicit := - declaration is used,
-					// golang will reuse the underlying object, and overwrite the object while it's being tested
-					// by a separate thread.
-					// That is also the reason that blocktest within the struct is by-value instead of by-reference
-					var t testcase
-					t = testcase{blockTest: blocktest, name: name, filepath: path}
-					if err := t.validate(); err != nil {
-						log.Error("error", "err", err, "test", t.name)
-						continue
-					}
-					i = i + 1
-					out <- &t
-				}
-			}
-			return nil
-		})
-		log.Info("file iterator done", "files", j, "tests", i)
-		close(out)
-	}()
-	return out
+func main() {
+	suite := hivesim.Suite{
+		Name: "consensus",
+		Description: "The 'consensus' test suite executes BlockchainTests from the " +
+			"offical test repository (https://github.com/ethereum/tests). For every test, it starts an instance of the client, " +
+			"and makes it import the RLP blocks. After import phase, the node is queried about it's latest blocks, which is matched " +
+			"to the expected last blockhash according to the test.",
+	}
+	suite.Add(hivesim.TestSpec{
+		Name: "test file loader",
+		Description: "This is a meta-test. It loads the blockchain test files and " +
+			"launches the actual client tests. Any errors in test files will be reported " +
+			"through this test.",
+		Run: loaderTest,
+	})
+	hivesim.MustRunSuite(hivesim.New(), suite)
 }
 
-type blocktestExecutor struct {
-	api         common.TestSuiteHost
-	testSuiteID common.TestSuiteID
-	client      string
+// loaderTest loads the blockchain test files and spawns the client tests.
+func loaderTest(t *hivesim.T) {
+	clientTypes, err := t.Sim.ClientTypes()
+	if err != nil {
+		t.Fatal("can't get client types:", err)
+	}
+
+	parallelism := 16
+	if val, ok := os.LookupEnv("HIVE_PARALLELISM"); ok {
+		if p, err := strconv.Atoi(val); err != nil {
+			t.Logf("Warning: invalid HIVE_PARALLELISM value %q", val)
+		} else {
+			parallelism = p
+		}
+	}
+	testLimit := -1
+	if val, ok := os.LookupEnv("HIVE_SIMLIMIT"); ok {
+		if p, err := strconv.Atoi(val); err != nil {
+			t.Logf("Warning: invalid HIVE_SIMLIMIT value %q", val)
+		} else {
+			testLimit = p
+		}
+	}
+	t.Log("parallelism:", parallelism, "testlimit:", testLimit)
+
+	// Find the tests directory.
+	testPath, isset := os.LookupEnv("TESTPATH")
+	if !isset {
+		t.Fatal("$TESTPATH not set")
+	}
+	fileRoot := fmt.Sprintf("%s/BlockchainTests/", testPath)
+
+	// Spawn workers.
+	var wg sync.WaitGroup
+	var testCh = make(chan *testcase)
+	wg.Add(parallelism)
+	for i := 0; i < parallelism; i++ {
+		go func() {
+			defer wg.Done()
+			for test := range testCh {
+				t.Run(hivesim.TestSpec{
+					Name:        test.name,
+					Description: "Test source: " + testLink(test.filepath),
+					Run:         test.run,
+				})
+			}
+		}()
+	}
+
+	// Deliver test cases.
+	loadTests(t, fileRoot, testLimit, func(tc testcase) {
+		for _, client := range clientTypes {
+			tc := tc // shallow copy
+			tc.clientType = client
+			testCh <- &tc
+		}
+	})
+	close(testCh)
+
+	// Wait for workers to finish.
+	wg.Wait()
+}
+
+// testLink turns a test path into a link to the tests repo.
+func testLink(filepath string) string {
+	// Convert ./tests/BlockchainTests/InvalidBlocks/bcExpectSection/lastblockhash.json
+	// into
+	// BlockchainTests/InvalidBlocks/bcExpectSection/lastblockhash.json
+	path := strings.TrimPrefix(filepath, ".")
+	path = strings.TrimPrefix(path, "/tests/")
+	// Link to
+	// https://github.com/ethereum/tests/blob/develop/BlockchainTests/InvalidBlocks/bcExpectSection/lastblockhash.json
+	return fmt.Sprintf("https://github.com/ethereum/tests/blob/develop/%v", path)
+}
+
+// deliverTests loads the files in 'root', running the given function for each test.
+func loadTests(t *hivesim.T, root string, limit int, fn func(testcase)) {
+	var i, j = 0, 0
+	filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if limit >= 0 && i >= limit {
+			return filepath.SkipDir
+		}
+		if info.IsDir() {
+			return nil
+		}
+		if fname := info.Name(); !strings.HasSuffix(fname, ".json") {
+			return nil
+		}
+		var tests map[string]BlockTest
+		if err := common.LoadJSON(path, &tests); err != nil {
+			t.Logf("invalid test file: %v", err)
+			return nil
+		}
+
+		j++
+		for name, blocktest := range tests {
+			tc := testcase{blockTest: blocktest, name: name, filepath: path}
+			if err := tc.validate(); err != nil {
+				t.Errorf("test validation failed for %s: %v", tc.name, err)
+				continue
+			}
+			fn(tc)
+			i++
+		}
+		return nil
+	})
 }
 
 type testcase struct {
-	name      string
-	blockTest BlockTest
-
-	filepath string
+	name       string
+	clientType string
+	blockTest  BlockTest
+	filepath   string
 }
 
-// validate returns error if the network is not defined
+// validate returns error if the test's chain rules are not supported.
 func (t *testcase) validate() error {
 	net := t.blockTest.json.Network
 	if _, exist := ruleset[net]; !exist {
@@ -284,14 +355,67 @@ func (t *testcase) validate() error {
 	return nil
 }
 
+// run launches the client and runs the test case against it.
+func (tc *testcase) run(t *hivesim.T) {
+	start := time.Now()
+	root, genesis, _, blocks, err := tc.artefacts()
+	if err != nil {
+		t.Fatal("can't prepare artefacts:", err)
+	}
+
+	// update the parameters with test-specific stuff
+	env := map[string]string{
+		"HIVE_FORK_DAO_VOTE": "1",
+		"HIVE_CHAIN_ID":      "1",
+	}
+	tc.updateEnv(env)
+	genesisTarget := strings.Replace(genesis, root, "", 1)
+	files := map[string]string{
+		genesisTarget: genesis,
+	}
+	for _, filename := range blocks {
+		fileTarget := strings.Replace(filename, root, "", 1)
+		files[fileTarget] = filename
+	}
+
+	t1 := time.Now()
+	client := t.StartClient(tc.clientType, env, files)
+
+	t2 := time.Now()
+	genesisHash, err := getHash(client.RPC(), hexutil.EncodeBig(new(big.Int)))
+	if err != nil {
+		t.Fatal("can't check genesis:", err)
+	}
+	if err = tc.verifyGenesis(genesisHash); err != nil {
+		t.Fatal(err)
+	}
+
+	// verify postconditions
+	t3 := time.Now()
+	lastHash, err := getHash(client.RPC(), "latest")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err = tc.verifyBestblock(lastHash); err != nil {
+		t.Fatal(err)
+	}
+
+	t4 := time.Now()
+	t.Logf(`test timing:
+  artefacts    %v
+  startClient  %v
+  checkGenesis %v
+  checkLatest  %v`, t1.Sub(start), t2.Sub(t1), t3.Sub(t2), t4.Sub(t3))
+}
+
 // updateEnv sets environment variables from the test
 func (t *testcase) updateEnv(env map[string]string) {
-	// Environment variables for rules
+	// Environment variables for rules.
 	rules := ruleset[t.blockTest.json.Network]
 	for k, v := range rules {
 		env[k] = fmt.Sprintf("%d", v)
 	}
-	// Possibly disable POW
+	// Possibly disable POW.
 	if t.blockTest.json.SealEngine == "NoProof" {
 		env["HIVE_SKIP_POW"] = "1"
 	}
@@ -327,11 +451,11 @@ func (t *testcase) artefacts() (string, string, string, []string, error) {
 	genBytes, _ := json.Marshal(genesis)
 	genesisFile := fmt.Sprintf("./%v/genesis.json", key)
 	if err := ioutil.WriteFile(genesisFile, genBytes, 0777); err != nil {
-		return "", "", "", nil, fmt.Errorf("Failed writing genesis: `%v`", err)
+		return "", "", "", nil, fmt.Errorf("failed writing genesis: %v", err)
 	}
 
 	for i, block := range t.blockTest.json.Blocks {
-		rlpdata := common2.FromHex(block.Rlp)
+		rlpdata := common.FromHex(block.Rlp)
 		fname := fmt.Sprintf("%s/%04d.rlp", blockFolder, i+1)
 		blocks = append(blocks, fname)
 		if err := ioutil.WriteFile(fname, rlpdata, 0777); err != nil {
@@ -343,119 +467,16 @@ func (t *testcase) artefacts() (string, string, string, []string, error) {
 }
 
 func (t *testcase) verifyGenesis(got []byte) error {
-	if exp := t.blockTest.json.Genesis.Hash; bytes.Compare(exp[:], got) != 0 {
-		return fmt.Errorf("Genesis mismatch, expected `0x%x` got `0x%x`", exp, got)
+	if exp := t.blockTest.json.Genesis.Hash; !bytes.Equal(exp[:], got) {
+		return fmt.Errorf("genesis mismatch:\n  want 0x%x\n   got 0x%x", exp, got)
 	}
 	return nil
 }
+
 func (t *testcase) verifyBestblock(got []byte) error {
-	if exp := t.blockTest.json.BestBlock; bytes.Compare(exp[:], got) != 0 {
-		return fmt.Errorf("Last block mismatch, expected `0x%x` got `0x%x` (`%v` `%v`)", exp, got)
+	if exp := t.blockTest.json.BestBlock; !bytes.Equal(exp[:], got) {
+		return fmt.Errorf("last block mismatch:\n  want 0x%x\n   got 0x%x", exp, got)
 	}
-	return nil
-}
-
-func (be *blocktestExecutor) run(testChan chan *testcase) {
-	var i = 0
-	for t := range testChan {
-
-		if err := be.runTest(t); err != nil {
-			log.Error("error", "err", err)
-		}
-		i++
-
-	}
-	log.Info("executor finished", "num_executed", i)
-}
-
-func (be *blocktestExecutor) runTest(t *testcase) error {
-
-	log.Info("Starting test", "name", t.name, "file", t.filepath)
-	start := time.Now()
-	// Convert ./tests/BlockchainTests/InvalidBlocks/bcExpectSection/lastblockhash.json
-	// into
-	// BlockchainTests/InvalidBlocks/bcExpectSection/lastblockhash.json
-	filePath := strings.TrimPrefix(t.filepath, ".")
-	filePath = strings.TrimPrefix(filePath, "/tests/")
-	// Link to
-	// https://github.com/ethereum/tests/blob/develop/BlockchainTests/InvalidBlocks/bcExpectSection/lastblockhash.json
-	//testID, err := be.api.StartTest(be.testSuiteID, t.name, testname)
-	description := fmt.Sprintf("Test source:[`%v`](https://github.com/ethereum/tests/blob/develop/%v)",
-		t.name,
-		filePath)
-
-	testID, err := be.api.StartTest(be.testSuiteID, t.name, description)
-	if err != nil {
-		return err
-	}
-
-	var done = func() {
-		var (
-			errString = ""
-			success   = (err == nil)
-		)
-		if !success {
-			errString = err.Error()
-		}
-		be.api.EndTest(be.testSuiteID, testID, &common.TestResult{success, errString}, nil)
-	}
-	defer done()
-
-	root, genesis, _, blocks, err := t.artefacts()
-	genesisTarget := strings.Replace(genesis, root, "", 1)
-	if err != nil {
-		return err
-	}
-	env := map[string]string{
-		"CLIENT":             be.client,
-		"HIVE_FORK_DAO_VOTE": "1",
-		"HIVE_CHAIN_ID":      "1",
-	}
-	files := map[string]string{
-		genesisTarget: genesis,
-	}
-
-	for _, filename := range blocks {
-		fileTarget := strings.Replace(filename, root, "", 1)
-		files[fileTarget] = filename
-	}
-	// update the parameters with test-specific stuff
-	t.updateEnv(env)
-	t1 := time.Now()
-	// spin up a node
-	_, ip, _, err := be.api.GetNode(be.testSuiteID, testID, env, files)
-	if err != nil {
-		return err
-	}
-	t2 := time.Now()
-	ctx := context.Background()
-	rawClient, err := rpc.DialContext(ctx, fmt.Sprintf("http://%s:8545", ip.String()))
-	if err != nil {
-		err = fmt.Errorf("Failed to start client: `%v`", err)
-		return err
-	}
-	genesisHash, err := getHash(rawClient, hexutil.EncodeBig(new(big.Int)))
-	if err != nil {
-		err = fmt.Errorf("Failed to check genesis: `%v`", err)
-		return err
-	}
-	t3 := time.Now()
-
-	if err = t.verifyGenesis(genesisHash); err != nil {
-		return err
-	}
-	t4 := time.Now()
-	// verify postconditions
-	lastHash, err := getHash(rawClient, "latest")
-	if err != nil {
-		return err
-	}
-	t5 := time.Now()
-	if err = t.verifyBestblock(lastHash); err != nil {
-		return err
-	}
-	t6 := time.Now()
-	log.Info("Test done", "name", t.name, "artefacts", t1.Sub(start), "newnode", t2.Sub(t1), "getGenesis", t3.Sub(t2), "verifyGenesis", t4.Sub(t3), "getLatest", t5.Sub(t4), "verifyLatest", t6.Sub(t5))
 	return nil
 }
 
@@ -463,84 +484,18 @@ func getHash(rawClient *rpc.Client, arg string) ([]byte, error) {
 	blockData := make(map[string]interface{})
 	if err := rawClient.Call(&blockData, "eth_getBlockByNumber", arg, false); err != nil {
 		// Make one more attempt
-		log.Info("Client connect failed, making one more attempt...")
+		fmt.Println("Client connect failed, making one more attempt...")
 		if err = rawClient.Call(&blockData, "eth_getBlockByNumber", arg, false); err != nil {
 			return nil, err
 		}
 	}
 	if hash, exist := blockData["hash"]; !exist {
-		return nil, fmt.Errorf("No hash found in response")
+		return nil, fmt.Errorf("no block hash found in response")
 	} else {
 		if hexHash, ok := hash.(string); !ok {
 			return nil, fmt.Errorf("error: string conversion failed for `%v`", hash)
 		} else {
-			return common2.HexToHash(hexHash).Bytes(), nil
+			return common.HexToHash(hexHash).Bytes(), nil
 		}
-	}
-}
-func main() {
-	paralellism := 16
-	log.Root().SetHandler(log.StdoutHandler)
-	if val, ok := os.LookupEnv("HIVE_PARALLELISM"); ok {
-		if p, err := strconv.Atoi(val); err != nil {
-			log.Warn("Hive paralellism could not be converted to int", "error", err)
-		} else {
-			paralellism = p
-		}
-	}
-	testLimit := -1
-	if val, ok := os.LookupEnv("HIVE_SIMLIMIT"); ok {
-		if p, err := strconv.Atoi(val); err != nil {
-			log.Warn("Simulator test limit could not be converted to int", "error", err)
-		} else {
-			testLimit = p
-		}
-	}
-	log.Info("Hive simulator started.", "paralellism", paralellism, "testlimit", testLimit)
-
-	// get the test suite engine provider and initialise
-	host := hive.New()
-
-	testpath, isset := os.LookupEnv("TESTPATH")
-	if !isset {
-		log.Error("Test path not set ($TESTPATH)")
-		os.Exit(1)
-	}
-
-	availableClients, _ := host.GetClientTypes()
-
-	log.Info("Got clients", "clients", availableClients)
-	logFile, _ := os.LookupEnv("HIVE_SIMLOG")
-
-	fileRoot := fmt.Sprintf("%s/BlockchainTests/", testpath)
-	for _, client := range availableClients {
-		testSuiteID, err := host.StartTestSuite("Consensus",
-			fmt.Sprintf("The `consensus` test suite executes BlockchainTests from the "+
-				"offical test repository: [here](https://github.com/ethereum/tests). For every test, it starts an instance of the client-under-test, "+
-				"(in this case, `%v`), and makes it import the RLP blocks. After import phase, the node is queried about it's latest blocks, which is matched"+
-				"to the expected last blockhash according to the test.", client), logFile)
-		if err != nil {
-			log.Error("Unable to start test suite", "error", err)
-			os.Exit(1)
-		}
-		defer func() {
-			if err := host.EndTestSuite(testSuiteID); err != nil {
-				log.Error("Unable to end test suite", "error", err)
-				os.Exit(1)
-			}
-		}()
-
-		testCh := deliverTests(fileRoot, testLimit)
-		var wg sync.WaitGroup
-		for i := 0; i < paralellism; i++ {
-			wg.Add(1)
-			go func() {
-				b := blocktestExecutor{api: host, testSuiteID: testSuiteID, client: client}
-				b.run(testCh)
-				wg.Done()
-			}()
-		}
-		log.Info("Tests started", "num threads", paralellism)
-		wg.Wait()
 	}
 }
