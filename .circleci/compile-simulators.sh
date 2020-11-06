15eb63d59bad81e578fd35246947e744b486709a
Felix Lange <fjl@twurst.com>
2020-11-06 18:37:11 +0100

    .circleci: try to fix it again

--- .circleci/compile-simulators.sh
+++ .circleci/compile-simulators.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-failed=
+failed=""
 sims=$(find simulators/ -name go.mod -printf '%h\n')
 for d in $sims; do
     echo "building $d"
@@ -13,4 +13,6 @@ for d in $sims; do
 done
 
 # Exit with non-zero status code if any build failed.
-if [ -n $failed ]; then exit 1; fi
+if [ -n "$failed" ]; then
+    exit 1
+fi
