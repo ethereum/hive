a7df3c029f9942a8cc05d6fa1593186d086c5c26
Felix Lange <fjl@twurst.com>
2020-11-06 17:49:40 +0100

    simulators/ethereum/rpc: make RPC simulation compile again  (#395)
    This updates the ethclient test suite to use the newer Hive simulation
    API. Unfortunately, most tests don't pass yet because they require
    ethash mining. This will be fixed later by introducing Clique mining
    support in clients.

--- simulators/ethereum/rpc/vault.go
+++ simulators/ethereum/rpc/vault.go
@@ -4,19 +4,17 @@ import (
 	"context"
 	"fmt"
 	"math/big"
-	"strings"
-	"testing"
-	"time"
-
 	"os"
+	"strings"
+	"time"
 
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/accounts"
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/accounts/keystore"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
 )
 
 var (
@@ -50,46 +48,28 @@ contract Vault {
 	defaultPassword = ""
 )
 
+var (
+	testChainID      = big.NewInt(7)
+	vaultAccountAddr = common.HexToAddress("0xcf49fda3be353c69b41ed96333cd24302da4556f")
+	vaultKey, _      = crypto.HexToECDSA("63b508a03c3b5937ceb903af8b1b0c191012ef6eb7e9c3fb7afa94e5d214d376")
+)
+
 func init() {
 	keyStore = keystore.NewKeyStore(os.TempDir(), keystore.StandardScryptN, keystore.StandardScryptP)
-	accountsManager = accounts.NewManager(keyStore)
-}
-
-// nodeAddress returns the first account from the node and unlocks it.
-func nodeAddress(t *testing.T, client *TestClient) common.Address {
-	ctx, _ := context.WithTimeout(context.Background(), rpcTimeout)
-	var accounts []common.Address
-	if err := client.CallContext(ctx, &accounts, "eth_accounts"); err != nil {
-		panic(err)
-	}
-
-	addr := accounts[0]
-
-	ctx, _ = context.WithTimeout(context.Background(), rpcTimeout)
-	var success bool
-	if err := client.CallContext(ctx, &success, "personal_unlockAccount", addr, "", 3600); err != nil {
-		t.Fatalf("Unable to unlock account 0x%x: %v", addr, err)
-	}
-	if !success {
-		t.Fatalf("Unable to unlock node account 0x%x", addr)
-	}
-
-	return addr
+	accountsManager = accounts.NewManager(&accounts.Config{}, keyStore)
 }
 
 // createAndFundAccount creates a new account that is funded from the vault contract.
 // It will panic when the account could not be created and funded.
-func createAndFundAccountWithSubscription(t *testing.T, amount *big.Int, client *TestClient) accounts.Account {
+func createAndFundAccountWithSubscription(t *TestEnv, amount *big.Int) accounts.Account {
 	account, err := keyStore.NewAccount(defaultPassword)
 	if err != nil {
 		panic(err)
 	}
 
 	// each node has at least one 1 key with some of pre-allocated ether
-	fromAddr := nodeAddress(t, client)
-
 	if amount == nil {
-		amount = common.Big0
+		amount = big0
 	}
 
 	// setup subscriptions
@@ -99,49 +79,32 @@ func createAndFundAccountWithSubscription(t *testing.T, amount *big.Int, client 
 		heads    = make(chan *types.Header)
 		logsSub  ethereum.Subscription
 		logs     = make(chan types.Log)
-
 		vault, _ = abi.JSON(strings.NewReader(predeployedVaultABI))
 	)
 
-	ctx, _ = context.WithTimeout(context.Background(), rpcTimeout)
-	headsSub, err = client.SubscribeNewHead(ctx, heads)
+	headsSub, err = t.Eth.SubscribeNewHead(ctx, heads)
 	if err != nil {
 		panic(fmt.Sprintf("Could not create new head subscription: %v", err))
 	}
 	defer headsSub.Unsubscribe()
 
-	eventTopic := vault.Events["Send"].Id()
+	eventTopic := vault.Events["Send"].ID
 	addressTopic := common.BytesToHash(common.LeftPadBytes(account.Address[:], 32))
-	ctx, _ = context.WithTimeout(context.Background(), rpcTimeout)
 	q := ethereum.FilterQuery{
 		Addresses: []common.Address{predeployedVaultAddr},
 		Topics:    [][]common.Hash{[]common.Hash{eventTopic}, []common.Hash{addressTopic}},
 	}
-	logsSub, err = client.SubscribeFilterLogs(ctx, q, logs)
+	logsSub, err = t.Eth.SubscribeFilterLogs(ctx, q, logs)
 	if err != nil {
 		panic(fmt.Sprintf("Could not create log filter subscription: %v", err))
 	}
 	defer logsSub.Unsubscribe()
 
 	// order the vault to send some ether
-	payload, err := vault.Pack("sendSome", account.Address, amount)
-	if err != nil {
-		t.Fatalf("Unable to send some ether to new account: %v", err)
+	tx := vaultSendSome(t, account.Address, amount)
+	if err := t.Eth.SendTransaction(t.Ctx(), tx); err != nil {
+		t.Fatalf("unable to send funding transaction: %v", err)
 	}
-
-	txPayload := map[string]interface{}{
-		"from": fromAddr,
-		"to":   predeployedVaultAddr,
-		"data": hexutil.Bytes(payload),
-		"gas":  hexutil.EncodeBig(big.NewInt(75000)),
-	}
-
-	ctx, _ = context.WithTimeout(context.Background(), rpcTimeout)
-	var txHash common.Hash
-	if err := client.CallContext(ctx, &txHash, "eth_sendTransaction", txPayload); err != nil {
-		t.Fatalf("Unable to send funding transaction: %v", err)
-	}
-
 	var (
 		latestHeader *types.Header
 		receivedLog  *types.Log
@@ -156,15 +119,16 @@ func createAndFundAccountWithSubscription(t *testing.T, amount *big.Int, client 
 		case log := <-logs:
 			if !log.Removed {
 				receivedLog = &log
-			} else if log.Removed && receivedLog != nil && receivedLog.BlockHash == log.BlockHash { // chain reorg
+			} else if log.Removed && receivedLog != nil && receivedLog.BlockHash == log.BlockHash {
+				// chain reorg!
 				receivedLog = nil
 			}
 		case err := <-headsSub.Err():
 			t.Fatalf("Could not fund new account: %v", err)
 		case err := <-logsSub.Err():
-			t.Fatalf("Could not not fund new account: %v", err)
+			t.Fatalf("Could not fund new account: %v", err)
 		case <-timeout.C:
-			t.Fatal("Could not not fund new account: timeout")
+			t.Fatal("Could not fund new account: timeout")
 		}
 
 		if latestHeader != nil && receivedLog != nil {
@@ -177,53 +141,40 @@ func createAndFundAccountWithSubscription(t *testing.T, amount *big.Int, client 
 	return account
 }
 
+func nextNonce(t *TestEnv, account common.Address) uint64 {
+	nonce, err := t.Eth.NonceAt(t.Ctx(), account, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return nonce
+}
+
 // createAndFundAccount creates a new account that is funded from the vault contract.
 // It will panic when the account could not be created and funded.
-func createAndFundAccount(t *testing.T, amount *big.Int, client *TestClient) accounts.Account {
+func createAndFundAccount(t *TestEnv, amount *big.Int) accounts.Account {
 	account, err := keyStore.NewAccount(defaultPassword)
 	if err != nil {
 		panic(err)
 	}
-
-	// each node has at least one 1 key with some of pre-allocated ether
-	fromAddr := nodeAddress(t, client)
-
 	if amount == nil {
-		amount = common.Big0
+		amount = big0
 	}
 
 	// order the vault to send some ether
-	vault, _ := abi.JSON(strings.NewReader(predeployedVaultABI))
-	payload, err := vault.Pack("sendSome", account.Address, amount)
-	if err != nil {
-		t.Fatalf("Unable to send some ether to new account: %v", err)
-	}
-
-	txPayload := map[string]interface{}{
-		"from": fromAddr,
-		"to":   predeployedVaultAddr,
-		"data": hexutil.Bytes(payload),
-		"gas":  hexutil.EncodeBig(big.NewInt(75000)),
-	}
-
-	ctx, _ := context.WithTimeout(context.Background(), rpcTimeout)
-	var txHash common.Hash
-	if err := client.CallContext(ctx, &txHash, "eth_sendTransaction", txPayload); err != nil {
-		t.Fatalf("Unable to send funding transaction: %v", err)
+	tx := vaultSendSome(t, account.Address, amount)
+	if err := t.Eth.SendTransaction(t.Ctx(), tx); err != nil {
+		t.Fatalf("unable to send funding transaction: %v", err)
 	}
 
 	// wait for vaultTxConfirmationCount confirmation by checking the balance vaultTxConfirmationCount blocks back.
 	// createAndFundAccountWithSubscription for a better solution using logs
 	for i := 0; i < 120; i++ {
-		ctx, _ := context.WithTimeout(context.Background(), rpcTimeout)
-		block, err := client.BlockByNumber(ctx, nil)
+		block, err := t.Eth.BlockByNumber(t.Ctx(), nil)
 		if err != nil {
 			panic(err)
 		}
-
 		if block.NumberU64() > vaultTxConfirmationCount {
-			ctx, _ := context.WithTimeout(context.Background(), rpcTimeout)
-			balance, err := client.BalanceAt(ctx, account.Address, new(big.Int).Sub(block.Number(), new(big.Int).SetUint64(vaultTxConfirmationCount)))
+			balance, err := t.Eth.BalanceAt(t.Ctx(), account.Address, new(big.Int).Sub(block.Number(), new(big.Int).SetUint64(vaultTxConfirmationCount)))
 			if err != nil {
 				panic(err)
 			}
@@ -231,9 +182,28 @@ func createAndFundAccount(t *testing.T, amount *big.Int, client *TestClient) acc
 				return account
 			}
 		}
-
 		time.Sleep(time.Second)
 	}
+	panic(fmt.Sprintf("Could not fund account 0x%x in transaction 0x%x", account.Address, tx.Hash()))
+}
 
-	panic(fmt.Sprintf("Could not fund account 0x%x in transaction 0x%x", account.Address, txHash))
+func vaultSendSome(t *TestEnv, recipient common.Address, amount *big.Int) *types.Transaction {
+	vault, _ := abi.JSON(strings.NewReader(predeployedVaultABI))
+	payload, err := vault.Pack("sendSome", recipient, amount)
+	if err != nil {
+		t.Fatalf("can't pack pack vault tx input: %v", err)
+	}
+	var (
+		nonce    = nextNonce(t, vaultAccountAddr)
+		gasLimit = uint64(75000)
+		gasPrice = new(big.Int)
+		txAmount = new(big.Int)
+	)
+	tx := types.NewTransaction(nonce, predeployedVaultAddr, txAmount, gasLimit, gasPrice, payload)
+	signer := types.NewEIP155Signer(testChainID)
+	signedTx, err := types.SignTx(tx, signer, vaultKey)
+	if err != nil {
+		t.Fatal("can't sign vault funding tx:", err)
+	}
+	return signedTx
 }
