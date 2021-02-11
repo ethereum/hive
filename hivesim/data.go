249e39c8236734e12697975165e82aaea13d3ab1
protolambda <proto@protolambda.com>
2021-02-11 23:39:01 +0100

    hivesim client start options and dynamic file sources

--- hivesim/data.go
+++ hivesim/data.go
@@ -17,6 +17,15 @@ type TestResult struct {
 // a global variable and then customize them for specific clients.
 type Params map[string]string
 
+var _ StartOption = (Params)(nil)
+
+// Apply adds the Params to the environment of a client start, making Params a StartOption.
+func (p Params) Apply(setup *clientSetup) {
+	for k, v := range p {
+		setup.parameters[k] = v
+	}
+}
+
 // Set returns a copy of the parameters with 'key' set to 'value'.
 func (p Params) Set(key, value string) Params {
 	cpy := p.Copy()
