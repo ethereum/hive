0460bfe9e44efcd8e1227c448c00d17eb1ca5c78
marioevz <marioevz@gmail.com>
2022-06-21 23:08:43 +0000

    clients/teku: add archive data storage param

--- clients/teku-bn/teku_bn.sh
+++ clients/teku-bn/teku_bn.sh
@@ -38,6 +38,8 @@ if [ "$HIVE_ETH2_MERGE_ENABLED" != "" ]; then
     merge_option="--ee-endpoint=$HIVE_ETH2_ETH1_ENGINE_RPC_ADDRS --ee-jwt-secret-file=/jwtsecret"
 fi
 
+echo Starting Teku Beacon Node
+
 /opt/teku/bin/teku \
     --network=/data/testnet_setup/config.yaml \
     --data-path=/data/teku \
@@ -51,4 +53,5 @@ fi
     --p2p-udp-port="${HIVE_ETH2_P2P_UDP_PORT:-9000}" \
     --p2p-advertised-ip="${CONTAINER_IP}" \
     --p2p-peer-lower-bound="${HIVE_ETH2_P2P_TARGET_PEERS:-10}" \
-    --rest-api-enabled=true --rest-api-interface=0.0.0.0 --rest-api-port="${HIVE_ETH2_BN_API_PORT:-4000}" --rest-api-host-allowlist="*"
+    --rest-api-enabled=true --rest-api-interface=0.0.0.0 --rest-api-port="${HIVE_ETH2_BN_API_PORT:-4000}" --rest-api-host-allowlist="*" \
+    --data-storage-mode=ARCHIVE
