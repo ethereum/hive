42671aad51b44ae38fb053e836b37aad5e57b95b
Igor Mandrigin <i@mandrigin.ru>
2022-07-04 18:09:40 +0200

    add debug logs

--- internal/libdocker/container.go
+++ internal/libdocker/container.go
@@ -218,15 +218,19 @@ func checkPort(ctx context.Context, logger log15.Logger, addr string, notify cha
 			conn, err := dialer.DialContext(ctx, "tcp", addr)
 			if err == nil {
 				defer conn.Close()
+				logger.Debug("port online, checking for RPC readiness...")
 
 				rpcClient, err2 := rpc.DialHTTP(addr)
 				if err2 == nil {
 					defer rpcClient.Close()
 					err3 := rpcClient.Call("eth_getBlockByNumber", "latest", false)
 					if err3 == nil {
+						logger.Debug("conteiner RPC ready")
 						close(notify)
+						return
 					}
 				}
+				logger.Debug("conteiner RPC not ready, waiting...")
 				return
 			}
 		}
