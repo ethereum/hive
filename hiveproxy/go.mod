75d4648a468c5fbf77c2b6794694cc931f6bcac7
Felix Lange <fjl@twurst.com>
2022-07-07 01:50:26 +0200

    hiveproxy: add CheckLive

--- hiveproxy/go.mod
+++ hiveproxy/go.mod
@@ -3,3 +3,18 @@ module github.com/ethereum/hive/hiveproxy
 go 1.17
 
 require github.com/hashicorp/yamux v0.0.0-20211028200310-0bc27b27de87
+
+require (
+	github.com/StackExchange/wmi v0.0.0-20180116203802-5d049714c4a6 // indirect
+	github.com/deckarep/golang-set v1.8.0 // indirect
+	github.com/ethereum/go-ethereum v1.10.20 // indirect
+	github.com/go-ole/go-ole v1.2.1 // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
+	github.com/tklauser/go-sysconf v0.3.5 // indirect
+	github.com/tklauser/numcpus v0.2.2 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
+)
--- hiveproxy/proxy.go
+++ hiveproxy/proxy.go
@@ -3,12 +3,16 @@ package hiveproxy
 import (
 	"context"
 	"embed"
+	"errors"
 	"io"
 	"net"
 	"net/http"
 	"net/http/httputil"
 	"sync"
+	"sync/atomic"
+	"time"
 
+	"github.com/ethereum/go-ethereum/rpc"
 	"github.com/hashicorp/yamux"
 )
 
@@ -17,13 +21,18 @@ var Source embed.FS
 
 type Proxy struct {
 	httpsrv    http.Server
+	rpc        *rpc.Client
 	serverDown chan struct{}
 	closeOnce  sync.Once
+
+	isFront bool
+	callID  uint64
 }
 
-func newProxy() *Proxy {
+func newProxy(front bool) *Proxy {
 	return &Proxy{
 		serverDown: make(chan struct{}),
+		isFront:    front,
 	}
 }
 
@@ -34,22 +43,67 @@ func (p *Proxy) Close() {
 	})
 }
 
+func (p *Proxy) CheckLive(ctx context.Context, addr string) error {
+	if p.isFront {
+		return errors.New("CheckLive called on proxy frontend")
+	}
+
+	id := atomic.AddUint64(&p.callID, 1)
+	checkDone := make(chan struct{})
+	cancelDone := p.backgroundCancelRPC(ctx, checkDone, id)
+	defer func() {
+		close(checkDone)
+		<-cancelDone
+	}()
+
+	return p.rpc.CallContext(ctx, nil, "proxy_checkLive", id, addr)
+}
+
+func (p *Proxy) backgroundCancelRPC(ctx context.Context, done <-chan struct{}, id uint64) chan struct{} {
+	cancelDone := make(chan struct{})
+	go func() {
+		defer close(cancelDone)
+		select {
+		case <-ctx.Done():
+			bgCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+			p.rpc.Notify(bgCtx, "proxy_cancel", id)
+			cancel()
+		case <-done:
+		}
+	}()
+	return cancelDone
+}
+
 func (p *Proxy) serve(l net.Listener) {
 	p.httpsrv.Serve(l)
 	close(p.serverDown)
 }
 
-func RunClient(r io.Reader, w io.WriteCloser, listener net.Listener) *Proxy {
+func (p *Proxy) launchRPC(stream net.Conn) {
+	p.rpc, _ = rpc.DialIO(context.Background(), stream, stream)
+}
+
+func RunFrontend(r io.Reader, w io.WriteCloser, listener net.Listener) *Proxy {
 	mux, _ := yamux.Client(rwCombo{r, w}, nil)
+	p := newProxy(true)
+
+	// Launch RPC handler.
+	rpcConn, err := mux.Open()
+	if err != nil {
+		panic(err)
+	}
+	p.launchRPC(rpcConn)
+	p.rpc.RegisterName("proxy", new(proxyFunctions))
+
+	// Launch reverse proxy server.
 	transport := &http.Transport{
 		DialContext: func(ctx context.Context, network, addr string) (net.Conn, error) {
 			return mux.Open()
 		},
 	}
-	p := newProxy()
 	p.httpsrv.Handler = &httputil.ReverseProxy{
 		Director: func(req *http.Request) {
-			// This is needed to satisy the transport."
+			// This is needed to make http.Transport forward the request.
 			req.URL.Scheme = "http"
 			req.URL.Host = "hive"
 		},
@@ -59,9 +113,18 @@ func RunClient(r io.Reader, w io.WriteCloser, listener net.Listener) *Proxy {
 	return p
 }
 
-func RunServer(r io.Reader, w io.WriteCloser, h http.Handler) *Proxy {
+func RunBackend(r io.Reader, w io.WriteCloser, h http.Handler) *Proxy {
 	mux, _ := yamux.Server(rwCombo{r, w}, nil)
-	p := newProxy()
+	p := newProxy(false)
+
+	// Start RPC client.
+	rpcConn, err := mux.Accept()
+	if err != nil {
+		panic(err)
+	}
+	p.launchRPC(rpcConn)
+
+	// Start HTTP server.
 	p.httpsrv.Handler = h
 	go p.serve(mux)
 	return p
