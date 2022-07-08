e809985581b93d20c14e829441f5e81033ea1d3c
Felix Lange <fjl@twurst.com>
2022-07-08 19:26:59 +0000

    internal/libdocker: launch proxy backend before starting proxy container

--- internal/libdocker/proxy.go
+++ internal/libdocker/proxy.go
@@ -29,16 +29,35 @@ func (cb *ContainerBackend) ServeAPI(ctx context.Context, h http.Handler) (libhi
 	if err != nil {
 		return nil, err
 	}
+
+	// Launch the proxy server before starting the container.
+	var (
+		proxy     *hiveproxy.Proxy
+		proxyErrC = make(chan error, 1)
+	)
+	go func() {
+		var err error
+		proxy, err = hiveproxy.RunBackend(outR, inW, h)
+		if err != nil {
+			log15.Error("proxy backend startup failed", "err", err)
+		}
+		proxyErrC <- err
+	}()
+
+	// Now start the container.
 	info, err := cb.StartContainer(ctx, id, opts)
 	if err != nil {
 		cb.DeleteContainer(id)
 		return nil, err
 	}
 
-	proxy, err := hiveproxy.RunBackend(outR, inW, h)
-	if err != nil {
-		cb.DeleteContainer(id)
-		return nil, err
+	// Proxy server should come up.
+	select {
+	case err := <-proxyErrC:
+		if err != nil {
+			cb.DeleteContainer(id)
+			return nil, err
+		}
 	}
 
 	// Register proxy in ContainerBackend, so it can be used for CheckLive.
