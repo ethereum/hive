0fc0cdaa4b342f94640681065d2ffcc12246caa4
protolambda <proto@protolambda.com>
2022-08-11 18:46:04 +0200

    optimism: add l1 genesis timestamp flag

--- clients/op-contracts/scripts/deploy_config.sh
+++ clients/op-contracts/scripts/deploy_config.sh
@@ -5,4 +5,4 @@ set -eu
 cd /hive/optimism/packages/contracts-bedrock
 
 # Deploy config is provided as first script argument
-echo "$1" > deploy-config/hivenet_data.json
+echo "$1" > deploy-config/hivenet.json
