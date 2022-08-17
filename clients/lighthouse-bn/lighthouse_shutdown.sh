8183f1bdeeead883a7c84d4fb2ff1a5ab477bdb4
marioevz <marioevz@gmail.com>
2022-08-17 15:57:33 +0000

    clients/lighthouse-bn: add shutdown

---
+++ clients/lighthouse-bn/lighthouse_shutdown.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Immediately abort the script on any error encountered
+set -e
+
+# Send CTRL+C to start the shutdown process
+kill -SIGINT $(pgrep lighthouse)
+
+# Wait for graceful shutdown
+TIMEOUT_SECONDS=20
+while pgrep lighthouse; do
+    if (( TIMEOUT_SECONDS == 0 )); then
+        break
+    fi
+    sleep 1
+    (( TIMEOUT_SECONDS-- ))
+done
+
+# Forcefully kill if it did not shutdown
+if pgrep lighthouse; then
+    kill -9 $(pgrep lighthouse)
+fi
