58892eaa409af340790958b15951bc94fa327107
protolambda <proto@protolambda.com>
2022-12-10 21:13:47 +0100

    hive: support shared modules with hive_context.txt and go workspace

--- internal/libdocker/builder.go
+++ internal/libdocker/builder.go
@@ -10,11 +10,13 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 
-	"github.com/ethereum/hive/internal/libhive"
 	docker "github.com/fsouza/go-dockerclient"
 	"gopkg.in/inconshreveable/log15.v2"
 	"gopkg.in/yaml.v3"
+
+	"github.com/ethereum/hive/internal/libhive"
 )
 
 // Builder takes care of building docker images.
@@ -57,15 +59,29 @@ func (b *Builder) BuildClientImage(ctx context.Context, name string) (string, er
 	dir := b.config.Inventory.ClientDirectory(name)
 	_, branch := libhive.SplitClientName(name)
 	tag := fmt.Sprintf("hive/clients/%s:latest", name)
-	err := b.buildImage(ctx, dir, branch, tag)
+	err := b.buildImage(ctx, dir, "Dockerfile", branch, tag)
 	return tag, err
 }
 
 // BuildSimulatorImage builds a docker image of a simulator.
 func (b *Builder) BuildSimulatorImage(ctx context.Context, name string) (string, error) {
 	dir := b.config.Inventory.SimulatorDirectory(name)
+	buildContextPath := dir
+	buildDockerfile := "Dockerfile"
+	// build context dir of simulator can be overridden with "hive_context.txt" file containing the desired build path
+	if contextPathBytes, err := os.ReadFile(filepath.Join(filepath.FromSlash(dir), "hive_context.txt")); err == nil {
+		buildContextPath = filepath.Join(dir, strings.TrimSpace(string(contextPathBytes)))
+		if strings.HasPrefix(buildContextPath, "../") {
+			return "", fmt.Errorf("cannot access build directory outside of Hive root: %q", buildContextPath)
+		}
+		if p, err := filepath.Rel(buildContextPath, filepath.Join(filepath.FromSlash(dir), "Dockerfile")); err != nil {
+			return "", fmt.Errorf("failed to derive relative simulator Dockerfile path: %v", err)
+		} else {
+			buildDockerfile = p
+		}
+	}
 	tag := fmt.Sprintf("hive/simulators/%s:latest", name)
-	err := b.buildImage(ctx, dir, "", tag)
+	err := b.buildImage(ctx, buildContextPath, buildDockerfile, "", tag)
 	return tag, err
 }
 
@@ -202,7 +218,7 @@ func (b *Builder) ReadFile(ctx context.Context, image, path string) ([]byte, err
 
 // buildImage builds a single docker image from the specified context.
 // branch specifes a build argument to use a specific base image branch or github source branch.
-func (b *Builder) buildImage(ctx context.Context, contextDir, branch, imageTag string) error {
+func (b *Builder) buildImage(ctx context.Context, contextDir, dockerFile, branch, imageTag string) error {
 	nocache := false
 	if b.config.NoCachePattern != nil {
 		nocache = b.config.NoCachePattern.MatchString(imageTag)
@@ -219,7 +235,7 @@ func (b *Builder) buildImage(ctx context.Context, contextDir, branch, imageTag s
 		Name:         imageTag,
 		ContextDir:   context,
 		OutputStream: ioutil.Discard,
-		Dockerfile:   "Dockerfile",
+		Dockerfile:   dockerFile,
 		NoCache:      nocache,
 		Pull:         b.config.PullEnabled,
 	}
