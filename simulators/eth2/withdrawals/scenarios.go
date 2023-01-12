4723e2faf8ea8dd7a6c8c061ef8eb977cc509ee1
marioevz <marioevz@gmail.com>
2023-01-11 18:06:38 -0600

    simulators/eth2/withdrawals: fixes and new tests

--- simulators/eth2/withdrawals/scenarios.go
+++ simulators/eth2/withdrawals/scenarios.go
@@ -10,10 +10,15 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/hive/hivesim"
 	"github.com/ethereum/hive/simulators/eth2/common/clients"
+	cl "github.com/ethereum/hive/simulators/eth2/common/config/consensus"
 	tn "github.com/ethereum/hive/simulators/eth2/common/testnet"
 	"github.com/protolambda/eth2api"
 	beacon "github.com/protolambda/zrnt/eth2/beacon/common"
 )
+
+var ConsensusClientsSupportingBLSChangesBeforeCapella = []string{
+	"prysm",
+}
 
 func TestCapellaPartialWithdrawalsWithoutBLSChanges(
 	t *hivesim.T, env *tn.Environment,
@@ -55,14 +60,25 @@ loop:
 
 type BLSToExecutionChangeTestSpec struct {
 	BaseWithdrawalsTestSpec
-	SubmitAfterCapellaFork bool
-	IgnoreRPCError         bool
+	SubmitBLSChangesOnBellatrix bool
+}
+
+func (ts BLSToExecutionChangeTestSpec) CanRun(
+	n clients.NodeDefinitions,
+) bool {
+	if !ts.SubmitBLSChangesOnBellatrix {
+		return true
+	}
+	filteredCLs := n.FilterByCL(
+		ConsensusClientsSupportingBLSChangesBeforeCapella,
+	)
+	return len(filteredCLs) > 0
 }
 
 func (ts BLSToExecutionChangeTestSpec) Execute(
 	t *hivesim.T,
 	env *tn.Environment,
-	n clients.NodeDefinition,
+	n []clients.NodeDefinition,
 ) {
 	config := ts.GetTestnetConfig(n)
 	ctx := context.Background()
@@ -87,8 +103,10 @@ func (ts BLSToExecutionChangeTestSpec) Execute(
 		if signedBlsChange, err := testnet.ValidatorClients().
 			SignBLSToExecutionChange(
 				capellaBLSToExecDomain,
-				beacon.ValidatorIndex(index),
-				executionAddress,
+				clients.BLSToExecutionChangeInfo{
+					ValidatorIndex: beacon.ValidatorIndex(index),
+					Eth1Address:    executionAddress,
+				},
 			); err != nil {
 			t.Fatalf("FAIL: Unable to sign bls-to-execution change: %v", err)
 		} else {
@@ -100,9 +118,8 @@ func (ts BLSToExecutionChangeTestSpec) Execute(
 	t.Logf("INFO: Prepared bls changes:\n%s", blsChangesJson)
 
 	// Send the signed bls changes to the beacon client
-	if !ts.SubmitAfterCapellaFork {
-		if err := testnet.BeaconClients().Running()[0].SubmitPoolBLSToExecutionChange(ctx, blsChanges); err != nil &&
-			!ts.IgnoreRPCError {
+	if ts.SubmitBLSChangesOnBellatrix {
+		if err := testnet.BeaconClients().Running()[0].SubmitPoolBLSToExecutionChange(ctx, blsChanges); err != nil {
 			t.Fatalf(
 				"FAIL: Unable to submit bls-to-execution changes: %v",
 				err,
@@ -117,8 +134,7 @@ func (ts BLSToExecutionChangeTestSpec) Execute(
 			testnet.WaitSlots(ctx, slotsUntilCapella)
 		}
 		// Then send the bls changes
-		if err := testnet.BeaconClients().Running()[0].SubmitPoolBLSToExecutionChange(ctx, blsChanges); err != nil &&
-			!ts.IgnoreRPCError {
+		if err := testnet.BeaconClients().Running()[0].SubmitPoolBLSToExecutionChange(ctx, blsChanges); err != nil {
 			t.Fatalf(
 				"FAIL: Unable to submit bls-to-execution changes: %v",
 				err,
@@ -164,21 +180,52 @@ func (ts BLSToExecutionChangeTestSpec) Execute(
 				credentials[12:],
 			)
 		}
-		t.Logf("INFO: Successful BLS to execution change: %x", credentials)
+		t.Logf("INFO: Successful BLS to execution change: %s", credentials)
 	}
 }
 
 type FullBLSToExecutionChangeTestSpec struct {
 	BaseWithdrawalsTestSpec
-	ValidatorsExitCount    uint64
-	SubmitAfterCapellaFork bool
-	IgnoreRPCError         bool
+	ValidatorsExitCount         uint64
+	SubmitBLSChangesOnBellatrix bool
+	SlashedValidators           bool
+}
+
+func (ts FullBLSToExecutionChangeTestSpec) CanRun(
+	n clients.NodeDefinitions,
+) bool {
+	if !ts.SubmitBLSChangesOnBellatrix {
+		return true
+	}
+	filteredCLs := n.FilterByCL(
+		ConsensusClientsSupportingBLSChangesBeforeCapella,
+	)
+	return len(filteredCLs) > 0
+}
+
+func (ts FullBLSToExecutionChangeTestSpec) GetValidatorKeys(
+	mnemonic string,
+) []*cl.KeyDetails {
+	keys := ts.BaseWithdrawalsTestSpec.GetValidatorKeys(mnemonic)
+	for i := uint64(0); i < ts.ValidatorsExitCount; i++ {
+		validatorIndex := beacon.ValidatorIndex(
+			i + (ts.GetValidatorCount() / 2) - (ts.ValidatorsExitCount / 2),
+		)
+		if ts.SlashedValidators {
+			keys[validatorIndex].Slashed = true
+		} else {
+			keys[validatorIndex].Exited = true
+		}
+		// Make validators' balances idiosyncratic to identify the withdrawal
+		keys[validatorIndex].ExtraInitialBalance = beacon.Gwei(i + 1)
+	}
+	return keys
 }
 
 func (ts FullBLSToExecutionChangeTestSpec) Execute(
 	t *hivesim.T,
 	env *tn.Environment,
-	n clients.NodeDefinition,
+	n []clients.NodeDefinition,
 ) {
 	config := ts.GetTestnetConfig(n)
 	ctx := context.Background()
@@ -194,76 +241,50 @@ func (ts FullBLSToExecutionChangeTestSpec) Execute(
 		3 * time.Second * time.Duration(testnet.Spec().SECONDS_PER_SLOT),
 	)
 
-	// Exit validators, and submit BLS-to-execution directives
+	// Submit BLS-to-execution directives
 	withdrawingValidators := make(
-		[]beacon.ValidatorIndex,
+		[]clients.BLSToExecutionChangeInfo,
 		ts.ValidatorsExitCount,
 	)
 	withdrawingValidatorsIndexes := make(
 		[]eth2api.ValidatorId,
 		ts.ValidatorsExitCount,
 	)
-	withdrawingAddresses := make(
-		[]beacon.Eth1Address,
-		ts.ValidatorsExitCount,
-	)
 	for i := uint64(0); i < ts.ValidatorsExitCount; i++ {
 		validatorIndex := beacon.ValidatorIndex(
 			i + (ts.GetValidatorCount() / 2) - (ts.ValidatorsExitCount / 2),
 		)
-		n := testnet.Nodes.ByValidatorIndex(validatorIndex)
-
-		if err := n.SignSubmitVoluntaryExit(ctx, beacon.Epoch(0), validatorIndex); err != nil {
-			// TODO: Debug this error because the exit is processed but we fail on
-			// parsing the response for some reason
-			// t.Fatalf("FAIL: unable to send voluntary exit: %v", err)
-		}
 
 		executionAddress := beacon.Eth1Address{byte(i + 0x100)}
 
-		withdrawingValidators[i] = validatorIndex
+		withdrawingValidators[i] = clients.BLSToExecutionChangeInfo{
+			ValidatorIndex: validatorIndex,
+			Eth1Address:    executionAddress,
+		}
 		withdrawingValidatorsIndexes[i] = eth2api.ValidatorIdIndex(
 			validatorIndex,
 		)
-		withdrawingAddresses[i] = executionAddress
 	}
 
 	// Send the signed bls changes to the beacon client
-	if !ts.SubmitAfterCapellaFork {
-		if err := testnet.Nodes.SignSubmitBLSToExecutionChanges(ctx, withdrawingValidators, withdrawingAddresses); err != nil {
-			if !ts.IgnoreRPCError {
-				t.Fatalf(
-					"FAIL: unable to submit bls-to-execution changes: %v",
-					err,
-				)
-			} else {
-				t.Logf(
-					"INFO: unable to submit bls-to-execution changes: %v",
-					err,
-				)
-			}
+	if ts.SubmitBLSChangesOnBellatrix {
+		if err := testnet.Nodes.SignSubmitBLSToExecutionChanges(ctx, withdrawingValidators); err != nil {
+			t.Fatalf(
+				"FAIL: unable to submit bls-to-execution changes: %v",
+				err,
+			)
 		}
 	} else {
 		// First wait for Capella
 		if config.CapellaForkEpoch.Uint64() > 0 {
-			slotsUntilCapella := beacon.Slot(
-				config.CapellaForkEpoch.Uint64(),
-			) * testnet.Spec().SLOTS_PER_EPOCH
-			testnet.WaitSlots(ctx, slotsUntilCapella)
+			testnet.WaitForFork(ctx, "capella")
 		}
 		// Then send the bls changes
-		if err := testnet.Nodes.SignSubmitBLSToExecutionChanges(ctx, withdrawingValidators, withdrawingAddresses); err != nil {
-			if !ts.IgnoreRPCError {
-				t.Fatalf(
-					"FAIL: unable to submit bls-to-execution changes: %v",
-					err,
-				)
-			} else {
-				t.Logf(
-					"INFO: unable to submit bls-to-execution changes: %v",
-					err,
-				)
-			}
+		if err := testnet.Nodes.SignSubmitBLSToExecutionChanges(ctx, withdrawingValidators); err != nil {
+			t.Fatalf(
+				"FAIL: unable to submit bls-to-execution changes: %v",
+				err,
+			)
 		}
 	}
 
@@ -273,6 +294,35 @@ loop:
 	for {
 		time.Sleep(time.Second * time.Duration(testnet.Spec().SECONDS_PER_SLOT))
 		testnet.BeaconClients().Running().PrintStatus(ctx, t)
+		// First check validators' statuses
+		if vals, err := testnet.BeaconClients().Running()[0].StateValidators(
+			ctx,
+			eth2api.StateHead,
+			withdrawingValidatorsIndexes,
+			nil,
+		); err != nil {
+			t.Fatalf("FAIL: Error getting validators' statuses: %v", err)
+		} else {
+			allExited := true
+			for i, val := range vals {
+				if val.Status != "exited_unslashed" &&
+					val.Status != "exited_voluntarily" &&
+					val.Status != "exited_slashed" &&
+					val.Status != "withdrawal_possible" &&
+					val.Status != "withdrawn" {
+					// if val.Status != eth2api.ValidatorStatusExitedVoluntarily {
+					t.Logf("INFO: validator %d has not exited yet: status=%s, exit epoch=%d", withdrawingValidatorsIndexes[i], val.Status, val.Validator.ExitEpoch)
+					allExited = false
+				}
+			}
+			if !allExited {
+				maxSlots -= 1
+				if maxSlots == 0 {
+					t.Fatalf("FAIL: Timeout waiting for full withdrawals")
+				}
+				continue
+			}
+		}
 		if balances, err := testnet.BeaconClients().
 			Running()[0].
 			StateValidatorBalances(
@@ -304,19 +354,19 @@ loop:
 	}
 
 	// Check the execution address balances
-	expectedMinimumBalance := big.NewInt(
-		int64(testnet.Spec().MAX_EFFECTIVE_BALANCE),
-	)
-	expectedMinimumBalance.Mul(expectedMinimumBalance, big.NewInt(1e9))
-	for _, addr := range withdrawingAddresses {
+	for i, valInfo := range withdrawingValidators {
 		address := common.Address{}
-		copy(address[:], addr[:])
+		copy(address[:], valInfo.Eth1Address[:])
 		if balance, err := testnet.ExecutionClients().Running()[0].BalanceAt(ctx, address, nil); err != nil {
 			t.Fatalf("FAIL: error obtaining account balance: %v", err)
 		} else {
 			// check balance is what we actually expect
-			if balance.Cmp(expectedMinimumBalance) < 0 {
-				t.Fatalf("FAIL: withdrawn balance less than minimum expected: want=%s, got=%s", expectedMinimumBalance.String(), balance.String())
+			expectedBalance := big.NewInt(
+				int64(testnet.Spec().MAX_EFFECTIVE_BALANCE) + int64(i) + 1,
+			)
+			expectedBalance.Mul(expectedBalance, big.NewInt(1e9))
+			if balance.Cmp(expectedBalance) != 0 {
+				t.Fatalf("FAIL: unexpected withdrawn balance: want=%s, got=%s", expectedBalance.String(), balance.String())
 			}
 		}
 	}
@@ -330,6 +380,7 @@ func TestCapellaFork(t *hivesim.T, env *tn.Environment,
 	testnet := tn.StartTestnet(ctx, t, env, config)
 	defer testnet.Stop()
 
+	// Wait for finality to do verifications
 	finalityCtx, cancel := testnet.Spec().EpochTimeoutContext(
 		ctx,
 		EPOCHS_TO_FINALITY+2,
