821eda9be0606afddab70acf516612b8d88b093f
marioevz <marioevz@gmail.com>
2023-01-13 09:36:39 -0600

    simulators/eth2/withdrawals: rework tests to use a single general function

--- simulators/eth2/withdrawals/main.go
+++ simulators/eth2/withdrawals/main.go
@@ -11,7 +11,6 @@ import (
 	"github.com/ethereum/hive/simulators/eth2/common/clients"
 	consensus_config "github.com/ethereum/hive/simulators/eth2/common/config/consensus"
 	"github.com/ethereum/hive/simulators/eth2/common/testnet"
-	beacon "github.com/protolambda/zrnt/eth2/beacon/common"
 )
 
 var (
@@ -30,112 +29,51 @@ type TestSpec interface {
 	GetDescription() string
 	Execute(*hivesim.T, *testnet.Environment, []clients.NodeDefinition)
 	GetValidatorKeys(string) []*consensus_config.KeyDetails
-	CanRun(clients.NodeDefinitions) bool
 }
 
 var tests = []TestSpec{
 	BaseWithdrawalsTestSpec{
 		Name: "test-capella-fork",
-		Run:  TestCapellaFork,
 		Description: `
 		Sanity test to check the fork transition to capella.
+		All validators start with Execution Withdrawal Credentials,
+		therefore no BLS-To-Execution changes are tested here.
 		`,
-		CapellaGenesis: false,
+		GenesisExecutionWithdrawalCredentialsShares: 1,
 	},
 
 	BaseWithdrawalsTestSpec{
 		Name: "test-capella-genesis",
-		Run:  TestCapellaFork,
 		Description: `
 		Sanity test to check the beacon clients can start with capella genesis.
+		All validators start with Execution Withdrawal Credentials,
+		therefore no BLS-To-Execution changes are tested here.
 		`,
 		CapellaGenesis: true,
-	},
-
-	BLSToExecutionChangeTestSpec{
-		BaseWithdrawalsTestSpec: BaseWithdrawalsTestSpec{
-			Name: "test-bls-to-execution-changes-on-capella",
-			Description: `
-			Send BLS-To-Execution-Changes after capella fork has happened.
-			`,
-			CapellaGenesis: true,
-		},
-	},
-
-	BLSToExecutionChangeTestSpec{
-		BaseWithdrawalsTestSpec: BaseWithdrawalsTestSpec{
-			Name: "test-bls-to-execution-changes-wait-for-capella-fork",
-			Description: `
-			Send BLS-To-Execution-Changes before capella fork has happened,
-			and verify they are included after the fork happens.
-			`,
-			CapellaGenesis: false,
-		},
-	},
-
-	BLSToExecutionChangeTestSpec{
-		BaseWithdrawalsTestSpec: BaseWithdrawalsTestSpec{
-			Name: "test-bls-to-execution-changes-on-bellatrix",
-			Description: `
-			Send BLS-To-Execution-Changes before capella fork has happened,
-			and verify they are included after the fork happens.
-			`,
-			CapellaGenesis: false,
-		},
-		SubmitBLSChangesOnBellatrix: true,
+		GenesisExecutionWithdrawalCredentialsShares: 1,
 	},
 
 	BaseWithdrawalsTestSpec{
-		Name: "test-partial-withdrawals-without-bls-to-execution-changes-bellatrix-genesis",
+		Name: "test-bls-to-execution-changes-on-capella",
 		Description: `
-		Test partial withdrawals from accounts that already had the
-		Eth1 withdrawal prefix since before the capella fork.
+		Send BLS-To-Execution-Changes after capella fork has happened.
+		Half of the validators still on BLS withdrawal credentials, and the
+		BLS-To-Execution-Change directives are sent for all the remaining
+		validators.
 		`,
-		Run:                            TestCapellaPartialWithdrawalsWithoutBLSChanges,
-		ValidatorsExtraBalance:         beacon.Gwei(1),
-		ExecutionWithdrawalCredentials: true,
-		CapellaGenesis:                 false,
+		GenesisExecutionWithdrawalCredentialsShares: 2,
+		SubmitBLSChangesOnBellatrix:                 true,
 	},
 
-	FullBLSToExecutionChangeTestSpec{
-		BaseWithdrawalsTestSpec: BaseWithdrawalsTestSpec{
-			Name: "test-full-withdrawals-exit-bls-to-execution-changes-bellatrix-genesis",
-			Description: `
+	BaseWithdrawalsTestSpec{
+		Name: "test-full-withdrawals-bls-to-execution-changes-bellatrix-genesis",
+		Description: `
 			Test BLS-To-Execution-Changes to fully exit validators.
 			`,
-			ValidatorCount:                 128,
-			ExecutionWithdrawalCredentials: false,
-			CapellaGenesis:                 false,
-		},
-		ValidatorsExitCount: 32,
-		SlashedValidators:   false,
-	},
-
-	FullBLSToExecutionChangeTestSpec{
-		BaseWithdrawalsTestSpec: BaseWithdrawalsTestSpec{
-			Name: "test-full-withdrawals-slashed-bls-to-execution-changes-bellatrix-genesis",
-			Description: `
-			Test BLS-To-Execution-Changes to fully exit validators.
-			`,
-			ValidatorCount:                 128,
-			ExecutionWithdrawalCredentials: false,
-			CapellaGenesis:                 false,
-		},
-		ValidatorsExitCount: 32,
-		SlashedValidators:   true,
-	},
-
-	FullBLSToExecutionChangeTestSpec{
-		BaseWithdrawalsTestSpec: BaseWithdrawalsTestSpec{
-			Name: "test-full-withdrawals-exit-bls-to-execution-changes-capella-genesis",
-			Description: `
-			Test BLS-To-Execution-Changes to fully exit validators.
-			`,
-			ValidatorCount:                 128,
-			ExecutionWithdrawalCredentials: false,
-			CapellaGenesis:                 true,
-		},
-		ValidatorsExitCount: 32,
+		GenesisExecutionWithdrawalCredentialsShares: 2,
+		GenesisExitedShares:                         8,
+		GenesisSlashedShares:                        8,
+		CapellaGenesis:                              false,
 	},
 }
 
@@ -172,30 +110,27 @@ func addAllTests(
 	clientCombinations := c.Combinations()
 	for _, test := range tests {
 		test := test
-		// Determine if we have the necessary clients to run
-		if test.CanRun(clientCombinations) {
-			suite.Add(hivesim.TestSpec{
-				Name: fmt.Sprintf(
-					"%s-%s",
-					test.GetName(),
-					strings.Join(clientCombinations.ClientTypes(), "-"),
-				),
-				Description: test.GetDescription(),
-				Run: func(t *hivesim.T) {
-					keys := test.GetValidatorKeys(mnemonic)
-					secrets, err := consensus_config.SecretKeys(keys)
-					if err != nil {
-						panic(err)
-					}
-					env := &testnet.Environment{
-						Clients: c,
-						Keys:    keys,
-						Secrets: secrets,
-					}
-					test.Execute(t, env, clientCombinations)
-				},
+		suite.Add(hivesim.TestSpec{
+			Name: fmt.Sprintf(
+				"%s-%s",
+				test.GetName(),
+				strings.Join(clientCombinations.ClientTypes(), "-"),
+			),
+			Description: test.GetDescription(),
+			Run: func(t *hivesim.T) {
+				keys := test.GetValidatorKeys(mnemonic)
+				secrets, err := consensus_config.SecretKeys(keys)
+				if err != nil {
+					panic(err)
+				}
+				env := &testnet.Environment{
+					Clients: c,
+					Keys:    keys,
+					Secrets: secrets,
+				}
+				test.Execute(t, env, clientCombinations)
 			},
-			)
-		}
+		},
+		)
 	}
 }
--- simulators/eth2/withdrawals/scenarios.go
+++ simulators/eth2/withdrawals/scenarios.go
@@ -3,14 +3,12 @@ package main
 import (
 	"bytes"
 	"context"
-	"encoding/json"
-	"math/big"
 	"time"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/hive/hivesim"
 	"github.com/ethereum/hive/simulators/eth2/common/clients"
-	cl "github.com/ethereum/hive/simulators/eth2/common/config/consensus"
+	"github.com/ethereum/hive/simulators/eth2/common/testnet"
 	tn "github.com/ethereum/hive/simulators/eth2/common/testnet"
 	"github.com/protolambda/eth2api"
 	beacon "github.com/protolambda/zrnt/eth2/beacon/common"
@@ -20,64 +18,11 @@ var ConsensusClientsSupportingBLSChangesBeforeCapella = []string{
 	"prysm",
 }
 
-func TestCapellaPartialWithdrawalsWithoutBLSChanges(
-	t *hivesim.T, env *tn.Environment,
-	config *tn.Config,
-) {
-	ctx := context.Background()
-
-	// config.CapellaForkEpoch = common.Big0
-	testnet := tn.StartTestnet(ctx, t, env, config)
-	defer testnet.Stop()
-
-	// Wait for beacon chain genesis to happen
-	testnet.WaitForGenesis(ctx)
-
-	// Wait for all validators to partially withdraw
-	slotsForAllPartialWithdrawals := beacon.Slot(
-		len(env.Keys) / int(testnet.Spec().MAX_WITHDRAWALS_PER_PAYLOAD),
-	)
-	slotCtx, cancel := testnet.Spec().
-		SlotTimeoutContext(ctx, slotsForAllPartialWithdrawals+1+(beacon.Slot(config.CapellaForkEpoch.Uint64())*testnet.Spec().SLOTS_PER_EPOCH))
-	defer cancel()
-loop:
-	for {
-		select {
-		case <-slotCtx.Done():
-			break loop
-		case <-time.After(time.Duration(testnet.Spec().SECONDS_PER_SLOT) * time.Second):
-			// Print all info
-			testnet.BeaconClients().Running().PrintStatus(slotCtx, t)
-		}
-	}
-
-	// Query the execution chain to check the balances, all most be !=0
-	ec := testnet.ExecutionClients().Running()[0]
-	if err := CheckCorrectWithdrawalBalances(ctx, ec, env.Keys); err != nil {
-		t.Fatalf("FAIL: balance incongruence found: %v", err)
-	}
-}
-
-type BLSToExecutionChangeTestSpec struct {
-	BaseWithdrawalsTestSpec
-	SubmitBLSChangesOnBellatrix bool
-}
-
-func (ts BLSToExecutionChangeTestSpec) CanRun(
-	n clients.NodeDefinitions,
-) bool {
-	if !ts.SubmitBLSChangesOnBellatrix {
-		return true
-	}
-	filteredCLs := n.FilterByCL(
-		ConsensusClientsSupportingBLSChangesBeforeCapella,
-	)
-	return len(filteredCLs) > 0
-}
-
-func (ts BLSToExecutionChangeTestSpec) Execute(
+// Generic withdrawals test routine, capable of running most of the test
+// scenarios.
+func (ts BaseWithdrawalsTestSpec) Execute(
 	t *hivesim.T,
-	env *tn.Environment,
+	env *testnet.Environment,
 	n []clients.NodeDefinition,
 ) {
 	config := ts.GetTestnetConfig(n)
@@ -85,6 +30,19 @@ func (ts BLSToExecutionChangeTestSpec) Execute(
 
 	testnet := tn.StartTestnet(ctx, t, env, config)
 	defer testnet.Stop()
+
+	blsDomain := ComputeBLSToExecutionDomain(testnet)
+
+	// Get all validators info
+	allValidators, err := ValidatorsFromBeaconState(
+		testnet.GenesisBeaconState(),
+		env.Keys,
+		&blsDomain,
+	)
+	if err != nil {
+		t.Fatalf("FAIL: Error parsing validators from beacon state")
+	}
+	genesisNonWithdrawable := allValidators.NonWithdrawable()
 
 	// Wait for beacon chain genesis to happen
 	testnet.WaitForGenesis(ctx)
@@ -94,57 +52,81 @@ func (ts BLSToExecutionChangeTestSpec) Execute(
 		3 * time.Second * time.Duration(testnet.Spec().SECONDS_PER_SLOT),
 	)
 
-	// Submit BLS-to-execution directives for all validators
-	capellaBLSToExecDomain := ComputeBLSToExecutionDomain(testnet)
+	if beaconClients := testnet.FilterByCL(ConsensusClientsSupportingBLSChangesBeforeCapella).
+		BeaconClients(); len(
+		beaconClients,
+	) > 0 &&
+		ts.SubmitBLSChangesOnBellatrix {
+		// If there are clients that support sending BLS to execution
+		// changes in bellatrix, we send half of the changes here
+		if len(genesisNonWithdrawable) > 0 {
+			nonWithdrawableValidators := genesisNonWithdrawable.Chunks(2)[0]
 
-	blsChanges := make(beacon.SignedBLSToExecutionChanges, 0)
-	for index := range env.Keys {
-		executionAddress := beacon.Eth1Address{byte(index + 0x100)}
-		if signedBlsChange, err := testnet.ValidatorClients().
-			SignBLSToExecutionChange(
-				capellaBLSToExecDomain,
-				clients.BLSToExecutionChangeInfo{
-					ValidatorIndex: beacon.ValidatorIndex(index),
-					Eth1Address:    executionAddress,
-				},
-			); err != nil {
-			t.Fatalf("FAIL: Unable to sign bls-to-execution change: %v", err)
+			if len(nonWithdrawableValidators) > 0 {
+				t.Logf(
+					"INFO: Sending %d validators' BLS-to-exec-change on bellatrix",
+					len(nonWithdrawableValidators),
+				)
+				for i := 0; i < len(nonWithdrawableValidators); i++ {
+					b := beaconClients[i%len(beaconClients)]
+					v := nonWithdrawableValidators[i]
+					if err := v.SignSendBLSToExecutionChange(
+						ctx,
+						b,
+						common.Address{byte(v.Index + 0x100)},
+					); err != nil {
+						t.Fatalf(
+							"FAIL: Unable to submit bls-to-execution changes: %v",
+							err,
+						)
+					}
+				}
+
+			}
+
 		} else {
-			blsChanges = append(blsChanges, *signedBlsChange)
-		}
-	}
-
-	blsChangesJson, _ := json.MarshalIndent(blsChanges, "", " ")
-	t.Logf("INFO: Prepared bls changes:\n%s", blsChangesJson)
-
-	// Send the signed bls changes to the beacon client
-	if ts.SubmitBLSChangesOnBellatrix {
-		if err := testnet.BeaconClients().Running()[0].SubmitPoolBLSToExecutionChange(ctx, blsChanges); err != nil {
-			t.Fatalf(
-				"FAIL: Unable to submit bls-to-execution changes: %v",
-				err,
-			)
+			t.Logf("INFO: no validators left on BLS credentials")
 		}
 	} else {
-		// First wait for Capella
-		if config.CapellaForkEpoch.Uint64() > 0 {
-			slotsUntilCapella := beacon.Slot(
-				config.CapellaForkEpoch.Uint64(),
-			) * testnet.Spec().SLOTS_PER_EPOCH
-			testnet.WaitSlots(ctx, slotsUntilCapella)
+		t.Logf("INFO: No beacon clients support BLS-To-Execution-Changes on bellatrix, skipping")
+	}
+
+	// Wait for Capella
+	if config.CapellaForkEpoch.Uint64() > 0 {
+		slotsUntilCapella := beacon.Slot(
+			config.CapellaForkEpoch.Uint64(),
+		) * testnet.Spec().SLOTS_PER_EPOCH
+		testnet.WaitSlots(ctx, slotsUntilCapella)
+	}
+
+	// If there are any remaining validators that cannot withdraw yet, send
+	// them now
+	nonWithdrawableValidators := allValidators.NonWithdrawable()
+	if len(nonWithdrawableValidators) > 0 {
+		beaconClients := testnet.BeaconClients()
+		for i := 0; i < len(nonWithdrawableValidators); i++ {
+			b := beaconClients[i%len(beaconClients)]
+			v := nonWithdrawableValidators[i]
+			if err := v.SignSendBLSToExecutionChange(
+				ctx,
+				b,
+				common.Address{byte(v.Index + 0x100)},
+			); err != nil {
+				t.Fatalf(
+					"FAIL: Unable to submit bls-to-execution changes: %v",
+					err,
+				)
+			}
 		}
-		// Then send the bls changes
-		if err := testnet.BeaconClients().Running()[0].SubmitPoolBLSToExecutionChange(ctx, blsChanges); err != nil {
-			t.Fatalf(
-				"FAIL: Unable to submit bls-to-execution changes: %v",
-				err,
-			)
-		}
+	} else {
+		t.Logf("INFO: no validators left on BLS credentials")
 	}
 
 	// Wait for all BLS to execution to be included
 	slotsForAllBlsInclusion := beacon.Slot(
-		len(env.Keys) / int(testnet.Spec().MAX_BLS_TO_EXECUTION_CHANGES),
+		len(genesisNonWithdrawable) / int(
+			testnet.Spec().MAX_BLS_TO_EXECUTION_CHANGES,
+		),
 	)
 	testnet.WaitSlots(ctx, slotsForAllBlsInclusion)
 
@@ -158,8 +140,8 @@ func (ts BLSToExecutionChangeTestSpec) Execute(
 		t.Fatalf("FAIL: Unable to get latest beacon state: %v", err)
 	}
 	validators := versionedBeaconState.Validators()
-	for index := range env.Keys {
-		validator := validators[index]
+	for _, v := range genesisNonWithdrawable {
+		validator := validators[v.Index]
 		credentials := validator.WithdrawalCredentials
 		if !bytes.Equal(
 			credentials[:1],
@@ -167,267 +149,52 @@ func (ts BLSToExecutionChangeTestSpec) Execute(
 		) {
 			t.Fatalf(
 				"FAIL: Withdrawal credential not updated for validator %d: %v",
-				index,
+				v.Index,
 				credentials,
 			)
 		}
-		expectedExecutionAddress := beacon.Eth1Address{byte(index + 0x100)}
-		if !bytes.Equal(expectedExecutionAddress[:], credentials[12:]) {
+		if v.WithdrawAddress == nil {
+			t.Fatalf(
+				"FAIL: BLS-to-execution change was not sent for validator %d",
+				v.Index,
+			)
+		}
+		if !bytes.Equal(v.WithdrawAddress[:], credentials[12:]) {
 			t.Fatalf(
 				"FAIL: Incorrect withdrawal credential for validator %d: want=%x, got=%x",
-				index,
-				expectedExecutionAddress,
+				v.Index,
+				v.WithdrawAddress,
 				credentials[12:],
 			)
 		}
 		t.Logf("INFO: Successful BLS to execution change: %s", credentials)
 	}
-}
 
-type FullBLSToExecutionChangeTestSpec struct {
-	BaseWithdrawalsTestSpec
-	ValidatorsExitCount         uint64
-	SubmitBLSChangesOnBellatrix bool
-	SlashedValidators           bool
-}
-
-func (ts FullBLSToExecutionChangeTestSpec) CanRun(
-	n clients.NodeDefinitions,
-) bool {
-	if !ts.SubmitBLSChangesOnBellatrix {
-		return true
-	}
-	filteredCLs := n.FilterByCL(
-		ConsensusClientsSupportingBLSChangesBeforeCapella,
+	// Wait for all validators to withdraw
+	slotsForAllPartialWithdrawals := beacon.Slot(
+		len(validators) / int(testnet.Spec().MAX_WITHDRAWALS_PER_PAYLOAD),
 	)
-	return len(filteredCLs) > 0
-}
-
-func (ts FullBLSToExecutionChangeTestSpec) GetValidatorKeys(
-	mnemonic string,
-) []*cl.KeyDetails {
-	keys := ts.BaseWithdrawalsTestSpec.GetValidatorKeys(mnemonic)
-	for i := uint64(0); i < ts.ValidatorsExitCount; i++ {
-		validatorIndex := beacon.ValidatorIndex(
-			i + (ts.GetValidatorCount() / 2) - (ts.ValidatorsExitCount / 2),
-		)
-		if ts.SlashedValidators {
-			keys[validatorIndex].Slashed = true
-		} else {
-			keys[validatorIndex].Exited = true
-		}
-		// Make validators' balances idiosyncratic to identify the withdrawal
-		keys[validatorIndex].ExtraInitialBalance = beacon.Gwei(i + 1)
-	}
-	return keys
-}
-
-func (ts FullBLSToExecutionChangeTestSpec) Execute(
-	t *hivesim.T,
-	env *tn.Environment,
-	n []clients.NodeDefinition,
-) {
-	config := ts.GetTestnetConfig(n)
-	ctx := context.Background()
-
-	testnet := tn.StartTestnet(ctx, t, env, config)
-	defer testnet.Stop()
-
-	// Wait for beacon chain genesis to happen
-	testnet.WaitForGenesis(ctx)
-
-	// Wait for 3 slots to pass
-	<-time.After(
-		3 * time.Second * time.Duration(testnet.Spec().SECONDS_PER_SLOT),
-	)
-
-	// Submit BLS-to-execution directives
-	withdrawingValidators := make(
-		[]clients.BLSToExecutionChangeInfo,
-		ts.ValidatorsExitCount,
-	)
-	withdrawingValidatorsIndexes := make(
-		[]eth2api.ValidatorId,
-		ts.ValidatorsExitCount,
-	)
-	for i := uint64(0); i < ts.ValidatorsExitCount; i++ {
-		validatorIndex := beacon.ValidatorIndex(
-			i + (ts.GetValidatorCount() / 2) - (ts.ValidatorsExitCount / 2),
-		)
-
-		executionAddress := beacon.Eth1Address{byte(i + 0x100)}
-
-		withdrawingValidators[i] = clients.BLSToExecutionChangeInfo{
-			ValidatorIndex: validatorIndex,
-			Eth1Address:    executionAddress,
-		}
-		withdrawingValidatorsIndexes[i] = eth2api.ValidatorIdIndex(
-			validatorIndex,
-		)
-	}
-
-	// Send the signed bls changes to the beacon client
-	if ts.SubmitBLSChangesOnBellatrix {
-		if err := testnet.Nodes.SignSubmitBLSToExecutionChanges(ctx, withdrawingValidators); err != nil {
-			t.Fatalf(
-				"FAIL: unable to submit bls-to-execution changes: %v",
-				err,
-			)
-		}
-	} else {
-		// First wait for Capella
-		if config.CapellaForkEpoch.Uint64() > 0 {
-			testnet.WaitForFork(ctx, "capella")
-		}
-		// Then send the bls changes
-		if err := testnet.Nodes.SignSubmitBLSToExecutionChanges(ctx, withdrawingValidators); err != nil {
-			t.Fatalf(
-				"FAIL: unable to submit bls-to-execution changes: %v",
-				err,
-			)
-		}
-	}
-
-	// Wait for all accounts to be drained or timeout
-	maxSlots := 16
+	slotCtx, cancel := testnet.Spec().
+		SlotTimeoutContext(ctx, slotsForAllPartialWithdrawals+5+(beacon.Slot(config.CapellaForkEpoch.Uint64())*testnet.Spec().SLOTS_PER_EPOCH))
+	defer cancel()
 loop:
 	for {
-		time.Sleep(time.Second * time.Duration(testnet.Spec().SECONDS_PER_SLOT))
-		testnet.BeaconClients().Running().PrintStatus(ctx, t)
-		// First check validators' statuses
-		if vals, err := testnet.BeaconClients().Running()[0].StateValidators(
-			ctx,
-			eth2api.StateHead,
-			withdrawingValidatorsIndexes,
-			nil,
-		); err != nil {
-			t.Fatalf("FAIL: Error getting validators' statuses: %v", err)
-		} else {
-			allExited := true
-			for i, val := range vals {
-				if val.Status != "exited_unslashed" &&
-					val.Status != "exited_voluntarily" &&
-					val.Status != "exited_slashed" &&
-					val.Status != "withdrawal_possible" &&
-					val.Status != "withdrawn" {
-					// if val.Status != eth2api.ValidatorStatusExitedVoluntarily {
-					t.Logf("INFO: validator %d has not exited yet: status=%s, exit epoch=%d", withdrawingValidatorsIndexes[i], val.Status, val.Validator.ExitEpoch)
-					allExited = false
+		select {
+		case <-slotCtx.Done():
+			t.Fatalf("FAIL: Timeout waiting on all accounts to withdraw")
+		case <-time.After(time.Duration(testnet.Spec().SECONDS_PER_SLOT) * time.Second):
+			// Print all info
+			testnet.BeaconClients().Running().PrintStatus(slotCtx, t)
+
+			// Check all accounts
+			for _, ec := range testnet.ExecutionClients().Running() {
+				if allAccountsWithdrawn, err := allValidators.Withdrawable().VerifyWithdrawnBalance(ctx, ec); err != nil {
+					t.Fatalf("FAIL: %v", err)
+				} else if allAccountsWithdrawn {
+					t.Logf("INFO: All accounts have successfully withdrawn")
+					break loop
 				}
 			}
-			if !allExited {
-				maxSlots -= 1
-				if maxSlots == 0 {
-					t.Fatalf("FAIL: Timeout waiting for full withdrawals")
-				}
-				continue
-			}
-		}
-		if balances, err := testnet.BeaconClients().
-			Running()[0].
-			StateValidatorBalances(
-				ctx,
-				eth2api.StateHead,
-				withdrawingValidatorsIndexes,
-			); err != nil {
-			t.Fatalf("FAIL: Error getting balances: %v", err)
-		} else {
-			allZero := true
-			for i, b := range balances {
-				if b.Balance != 0 {
-					t.Logf("INFO: validator %d balance not zero yet: %d", withdrawingValidatorsIndexes[i], b.Balance)
-					allZero = false
-					break
-				}
-			}
-			if allZero {
-				// All balances have dropped to zero
-				t.Logf("INFO: all exited validators have dropped their balance to zero")
-				break loop
-			}
-		}
-
-		maxSlots -= 1
-		if maxSlots == 0 {
-			t.Fatalf("FAIL: Timeout waiting for full withdrawals")
-		}
-	}
-
-	// Check the execution address balances
-	for i, valInfo := range withdrawingValidators {
-		address := common.Address{}
-		copy(address[:], valInfo.Eth1Address[:])
-		if balance, err := testnet.ExecutionClients().Running()[0].BalanceAt(ctx, address, nil); err != nil {
-			t.Fatalf("FAIL: error obtaining account balance: %v", err)
-		} else {
-			// check balance is what we actually expect
-			expectedBalance := big.NewInt(
-				int64(testnet.Spec().MAX_EFFECTIVE_BALANCE) + int64(i) + 1,
-			)
-			expectedBalance.Mul(expectedBalance, big.NewInt(1e9))
-			if balance.Cmp(expectedBalance) != 0 {
-				t.Fatalf("FAIL: unexpected withdrawn balance: want=%s, got=%s", expectedBalance.String(), balance.String())
-			}
-		}
-	}
-}
-
-func TestCapellaFork(t *hivesim.T, env *tn.Environment,
-	config *tn.Config,
-) {
-	ctx := context.Background()
-
-	testnet := tn.StartTestnet(ctx, t, env, config)
-	defer testnet.Stop()
-
-	// Wait for finality to do verifications
-	finalityCtx, cancel := testnet.Spec().EpochTimeoutContext(
-		ctx,
-		EPOCHS_TO_FINALITY+2,
-	)
-	defer cancel()
-
-	finalized, err := testnet.WaitForFinality(finalityCtx)
-	if err != nil {
-		t.Fatalf("FAIL: Waiting for finality: %v", err)
-	}
-
-	if err := testnet.VerifyParticipation(
-		ctx,
-		tn.FirstSlotAfterCheckpoint{Checkpoint: &finalized},
-		0.95,
-	); err != nil {
-		t.Fatalf("FAIL: Verifying participation: %v", err)
-	}
-
-	if err := testnet.VerifyExecutionPayloadIsCanonical(
-		ctx,
-		tn.LastSlotAtCheckpoint{Checkpoint: &finalized},
-	); err != nil {
-		t.Fatalf("FAIL: Verifying execution payload is canonical: %v", err)
-	}
-
-	if err := testnet.VerifyProposers(
-		ctx,
-		tn.LastSlotAtCheckpoint{Checkpoint: &finalized},
-		true,
-	); err != nil {
-		t.Fatalf("FAIL: Verifying proposers: %v", err)
-	}
-
-	if err := testnet.VerifyELHeads(ctx); err != nil {
-		t.Fatalf("FAIL: Verifying EL Heads: %v", err)
-	}
-
-	for _, el := range testnet.ExecutionClients().Running() {
-		if b, err := el.BlockByNumber(ctx, nil); err != nil {
-			t.Fatalf("FAIL: Unable to get execution client head: %v", err)
-		} else {
-			withdrawals := b.Header().WithdrawalsHash
-			if withdrawals == nil {
-				t.Fatalf("FAIL: Nil withdrawals on capella fork: %v", withdrawals)
-			}
-			t.Logf("INFO: Non-Nil withdrawals on capella fork: %v", withdrawals)
 		}
 	}
 }
