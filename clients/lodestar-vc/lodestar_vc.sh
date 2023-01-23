cdffec20c894cbca58e7903c1a7ada09be05aaae
marioevz <marioevz@gmail.com>
2023-01-23 07:40:50 -0600

    clients/lodestar: Fix vc

--- clients/lodestar-vc/lodestar_vc.sh
+++ clients/lodestar-vc/lodestar_vc.sh
@@ -21,7 +21,7 @@ do
   cp "/hive/input/secrets/$pubkey" "/data/secrets/$pubkey"
 done
 
-metrics_option=$([[ "$HIVE_ETH2_METRICS_PORT" == "" ]] && echo "" || echo "--metrics.enabled --metrics.address=$CONTAINER_IP --metrics.port=$HIVE_ETH2_METRICS_PORT")
+metrics_option=$([[ "$HIVE_ETH2_METRICS_PORT" == "" ]] && echo "" || echo "--metrics --metrics.address=$CONTAINER_IP --metrics.port=$HIVE_ETH2_METRICS_PORT")
 
 LOG=info
 case "$HIVE_LOGLEVEL" in
@@ -29,7 +29,7 @@ case "$HIVE_LOGLEVEL" in
     2)   LOG=warn  ;;
     3)   LOG=info  ;;
     4)   LOG=debug ;;
-    5)   LOG=silly ;;
+    5)   LOG=trace ;;
 esac
 
 echo Starting Lodestar Validator Client
@@ -37,10 +37,10 @@ echo Starting Lodestar Validator Client
 node /usr/app/node_modules/.bin/lodestar \
     validator \
     --logLevel="$LOG" \
-    --rootDir=/data/vc \
+    --dataDir=/data/vc \
     --paramsFile=/hive/input/config.yaml \
     --keystoresDir="/data/validators" \
     --secretsDir="/data/secrets" \
     $metrics_option \
-    --server="http://$HIVE_ETH2_BN_API_IP:$HIVE_ETH2_BN_API_PORT"
+    --beaconNodes="http://$HIVE_ETH2_BN_API_IP:$HIVE_ETH2_BN_API_PORT"
 
