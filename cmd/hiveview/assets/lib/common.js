07bb8ffb35cb297157fd0d97bcad7999b78977e0
Felix Lange <fjl@twurst.com>
2023-02-09 16:56:41 +0100

    cmd/hiveview: link to history instead of diff in header

--- cmd/hiveview/assets/lib/common.js
+++ cmd/hiveview/assets/lib/common.js
@@ -27,7 +27,7 @@ function hiveInfoHTML(data) {
         txt += '<span>built: ' + date + '</span>';
     }
     if (data.sourceCommit) {
-        let url = "https://github.com/ethereum/hive/commit/" + escape(data.sourceCommit);
+        let url = "https://github.com/ethereum/hive/commits/" + escape(data.sourceCommit);
         let link = '<a href="' + url + '">' + data.sourceCommit.substring(0, 8) + '</a>';
         txt += '<span>commit: ' + link + '</span>';
     }
