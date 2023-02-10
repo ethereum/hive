04625b8555899ff72fc22e919387717c83d7bbf7
marioevz <marioevz@gmail.com>
2023-02-10 12:20:49 -0600

    simulators/eth2/withdrawals: verify V1 endpoints indeed deprecated by beacon clients at capella

--- simulators/eth2/withdrawals/scenarios.go
+++ simulators/eth2/withdrawals/scenarios.go
@@ -8,6 +8,8 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/hive/hivesim"
 	"github.com/ethereum/hive/simulators/eth2/common/clients"
+
+	beacon_verification "github.com/ethereum/hive/simulators/eth2/common/spoofing/beacon"
 	tn "github.com/ethereum/hive/simulators/eth2/common/testnet"
 	"github.com/protolambda/eth2api"
 	beacon "github.com/protolambda/zrnt/eth2/beacon/common"
@@ -30,6 +32,25 @@ func (ts BaseWithdrawalsTestSpec) Execute(
 
 	testnet := tn.StartTestnet(ctx, t, env, config)
 	defer testnet.Stop()
+
+	// Add verification of Beacon->Execution Engine API calls to the proxies
+
+	// NewPayloadV1 expires at ShanghaiTime
+	newPayloadV1ExpireVerifier := beacon_verification.NewEngineMaxTimestampVerifier(
+		t,
+		beacon_verification.EngineNewPayloadV1,
+		testnet.ExecutionGenesis().Config.ShanghaiTime.Uint64(),
+	)
+	// ForkchoiceUpdatedV1 expires at ShanghaiTime
+	forkchoiceUpdatedV1ExpireVerifier := beacon_verification.NewEngineMaxTimestampVerifier(
+		t,
+		beacon_verification.EngineForkchoiceUpdatedV1,
+		testnet.ExecutionGenesis().Config.ShanghaiTime.Uint64(),
+	)
+	for _, e := range testnet.ExecutionClients() {
+		newPayloadV1ExpireVerifier.AddToProxy(e.Proxy())
+		forkchoiceUpdatedV1ExpireVerifier.AddToProxy(e.Proxy())
+	}
 
 	blsDomain := ComputeBLSToExecutionDomain(testnet)
 
