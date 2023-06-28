257a65b222f458e7be40f5cf1f9bb7a95e6bc17e
Mario Vega <marioevz@gmail.com>
2023-06-28 09:56:38 -0600

    clients/besu: Adding kzg file (#797)
    adding kzg file for besu
    
    Co-authored-by: parithosh <parithosh.jayanthi@ethereum.org>

--- clients/besu/besu.sh
+++ clients/besu/besu.sh
@@ -153,6 +153,11 @@ if [ "$HIVE_TERMINAL_TOTAL_DIFFICULTY" != "" ]; then
     RPCFLAGS="$RPCFLAGS --engine-host-allowlist=* --engine-jwt-enabled --engine-jwt-secret /jwtsecret"
 fi
 
+# Enable KZG trusted setup if cancun timestamp is set, needed for custom genesis on Besu wtih Cancun
+if [ "$HIVE_CANCUN_TIMESTAMP " != "" ]; then
+    FLAGS="$FLAGS --kzg-trusted-setup=/trusted_setup.txt"
+fi
+
 # Start Besu.
 if [ -z "$HAS_IMPORT" ]; then
     cmd="$besu $FLAGS $RPCFLAGS"
