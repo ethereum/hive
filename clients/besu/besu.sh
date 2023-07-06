5a6808e097aadeb60aa6b8a76b959de15ec644ae
Felix Lange <fjl@twurst.com>
2023-07-06 12:49:58 +0000

    clients/besu: fix issue with kzg-trusted-setup flag without cancun fork

--- clients/besu/besu.sh
+++ clients/besu/besu.sh
@@ -154,7 +154,7 @@ if [ "$HIVE_TERMINAL_TOTAL_DIFFICULTY" != "" ]; then
 fi
 
 # Enable KZG trusted setup if cancun timestamp is set, needed for custom genesis on Besu wtih Cancun
-if [ "$HIVE_CANCUN_TIMESTAMP " != "" ]; then
+if [ "$HIVE_CANCUN_TIMESTAMP" != "" ]; then
     FLAGS="$FLAGS --kzg-trusted-setup=/trusted_setup.txt"
 fi
 
