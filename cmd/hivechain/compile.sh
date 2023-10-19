b108895cd2a194ce534857d07935a68c41bb5a78
Felix Lange <fjl@twurst.com>
2023-10-19 15:31:28 +0200

    cmd/hivechain: add contract to test reverts

--- cmd/hivechain/compile.sh
+++ cmd/hivechain/compile.sh
@@ -5,6 +5,7 @@ set -xe
 geas -bin -no-push0 contracts/deployer.eas > bytecode/deployer.bin
 geas -bin -no-push0 contracts/callenv.eas > bytecode/callenv.bin
 geas -bin -no-push0 contracts/callme.eas > bytecode/callme.bin
+geas -bin -no-push0 contracts/callrevert.eas > bytecode/callrevert.bin
 geas -bin -no-push0 contracts/genlogs.eas > bytecode/genlogs.bin
 geas -bin -no-push0 contracts/gencode.eas > bytecode/gencode.bin
 geas -bin -no-push0 contracts/genstorage.eas > bytecode/genstorage.bin
