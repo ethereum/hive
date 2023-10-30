f4039a610628dfad55ebfee66be0ceb4d4ac95e9
Felix Lange <fjl@twurst.com>
2023-10-30 21:52:16 +0000

    clients/go-ethereum: remove -gcmode=archive in import

--- clients/go-ethereum/geth.sh
+++ clients/go-ethereum/geth.sh
@@ -40,7 +40,6 @@
 #
 #  - HIVE_MINER                   enable mining. value is coinbase address.
 #  - HIVE_MINER_EXTRA             extra-data field to set for newly minted blocks
-#  - HIVE_SKIP_POW                if set, skip PoW verification during block import
 #  - HIVE_LOGLEVEL                client loglevel (0-5)
 #  - HIVE_GRAPHQL_ENABLED         enables graphql on port 8545
 #  - HIVE_LES_SERVER              set to '1' to enable LES server
@@ -112,7 +111,7 @@ set +e
 # Load the test chain if present
 echo "Loading initial blockchain..."
 if [ -f /chain.rlp ]; then
-    $geth $FLAGS --gcmode=archive import /chain.rlp
+    $geth $FLAGS import /chain.rlp
 else
     echo "Warning: chain.rlp not found."
 fi
