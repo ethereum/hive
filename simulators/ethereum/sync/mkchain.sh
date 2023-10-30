73de82aa02248a7d6509d35be782e5599ee60678
Felix Lange <fjl@twurst.com>
2023-10-30 21:57:15 +0100

    simulators/ethereum/sync: update chain with merge at zero

--- simulators/ethereum/sync/mkchain.sh
+++ simulators/ethereum/sync/mkchain.sh
@@ -1,8 +1,8 @@
 
-hivechain generate \
+../../../hivechain generate \
     -outdir chain \
     -length 2000 \
     -lastfork shanghai \
     -tx-interval 5 \
-    -fork-interval 10 \
+    -fork-interval 0 \
     -outputs forkenv,genesis,chain,headblock,headfcu,headnewpayload
