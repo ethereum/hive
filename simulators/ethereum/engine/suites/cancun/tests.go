711a8419278ba6fb63875f5be30b32a3d6582585
Marcos Antonio Maceo Reyes <marcosmaceowork@gmail.com>
2023-10-31 13:03:51 +0400

    Uncomment working test cases

--- simulators/ethereum/engine/suites/cancun/tests.go
+++ simulators/ethereum/engine/suites/cancun/tests.go
@@ -28,132 +28,132 @@ func pUint64(v uint64) *uint64 {
 
 // List of all blob tests
 var Tests = []test.Spec{
-	//&CancunBaseSpec{
-	//
-	//	BaseSpec: test.BaseSpec{
-	//		Name: "Blob Transactions On Block 1, Shanghai Genesis",
-	//		About: `
-	//		Tests the Cancun fork since Block 1.
-	//
-	//		Verifications performed:
-	//		- Correct implementation of Engine API changes for Cancun:
-	//		  - engine_newPayloadV3, engine_forkchoiceUpdatedV3, engine_getPayloadV3
-	//		- Correct implementation of EIP-4844:
-	//		  - Blob transaction ordering and inclusion
-	//		  - Blob transaction blob gas cost checks
-	//		  - Verify Blob bundle on built payload
-	//		- Eth RPC changes for Cancun:
-	//		  - Blob fields in eth_getBlockByNumber
-	//		  - Beacon root in eth_getBlockByNumber
-	//		  - Blob fields in transaction receipts from eth_getTransactionReceipt
-	//		`,
-	//		MainFork: config.Cancun,
-	//		// We fork after genesis
-	//		ForkHeight: 1,
-	//	},
-	//
-	//	TestSequence: TestSequence{
-	//		// We are starting at Shanghai genesis so send a couple payloads to reach the fork
-	//		NewPayloads{},
-	//
-	//		// First, we send a couple of blob transactions on genesis,
-	//		// with enough data gas cost to make sure they are included in the first block.
-	//		SendBlobTransactions{
-	//			TransactionCount:              cancun.TARGET_BLOBS_PER_BLOCK,
-	//			BlobTransactionMaxBlobGasCost: big.NewInt(1000000000),
-	//		},
-	//
-	//		// We create the first payload, and verify that the blob transactions
-	//		// are included in the payload.
-	//		// We also verify that the blob transactions are included in the blobs bundle.
-	//		NewPayloads{
-	//			ExpectedIncludedBlobCount: cancun.TARGET_BLOBS_PER_BLOCK,
-	//			ExpectedBlobs:             helper.GetBlobList(0, cancun.TARGET_BLOBS_PER_BLOCK),
-	//		},
-	//
-	//		// Try to increase the data gas cost of the blob transactions
-	//		// by maxing out the number of blobs for the next payloads.
-	//		SendBlobTransactions{
-	//			TransactionCount:              DATA_GAS_COST_INCREMENT_EXCEED_BLOBS/(cancun.MAX_BLOBS_PER_BLOCK-cancun.TARGET_BLOBS_PER_BLOCK) + 1,
-	//			BlobsPerTransaction:           cancun.MAX_BLOBS_PER_BLOCK,
-	//			BlobTransactionMaxBlobGasCost: big.NewInt(1000000000),
-	//		},
-	//
-	//		// Next payloads will have max data blobs each
-	//		NewPayloads{
-	//			PayloadCount:              DATA_GAS_COST_INCREMENT_EXCEED_BLOBS / (cancun.MAX_BLOBS_PER_BLOCK - cancun.TARGET_BLOBS_PER_BLOCK),
-	//			ExpectedIncludedBlobCount: cancun.MAX_BLOBS_PER_BLOCK,
-	//		},
-	//
-	//		// But there will be an empty payload, since the data gas cost increased
-	//		// and the last blob transaction was not included.
-	//		NewPayloads{
-	//			ExpectedIncludedBlobCount: 0,
-	//		},
-	//
-	//		// But it will be included in the next payload
-	//		NewPayloads{
-	//			ExpectedIncludedBlobCount: cancun.MAX_BLOBS_PER_BLOCK,
-	//		},
-	//	},
-	//},
+	&CancunBaseSpec{
 
-	//&CancunBaseSpec{
-	//
-	//	BaseSpec: test.BaseSpec{
-	//		Name: "Blob Transactions On Block 1, Cancun Genesis",
-	//		About: `
-	//		Tests the Cancun fork since genesis.
-	//
-	//		Verifications performed:
-	//		* See Blob Transactions On Block 1, Shanghai Genesis
-	//		`,
-	//		MainFork: config.Cancun,
-	//	},
-	//
-	//	TestSequence: TestSequence{
-	//		NewPayloads{}, // Create a single empty payload to push the client through the fork.
-	//		// First, we send a couple of blob transactions on genesis,
-	//		// with enough data gas cost to make sure they are included in the first block.
-	//		SendBlobTransactions{
-	//			TransactionCount:              cancun.TARGET_BLOBS_PER_BLOCK,
-	//			BlobTransactionMaxBlobGasCost: big.NewInt(1000000000),
-	//		},
-	//
-	//		// We create the first payload, and verify that the blob transactions
-	//		// are included in the payload.
-	//		// We also verify that the blob transactions are included in the blobs bundle.
-	//		NewPayloads{
-	//			ExpectedIncludedBlobCount: cancun.TARGET_BLOBS_PER_BLOCK,
-	//			ExpectedBlobs:             helper.GetBlobList(0, cancun.TARGET_BLOBS_PER_BLOCK),
-	//		},
-	//
-	//		// Try to increase the data gas cost of the blob transactions
-	//		// by maxing out the number of blobs for the next payloads.
-	//		SendBlobTransactions{
-	//			TransactionCount:              DATA_GAS_COST_INCREMENT_EXCEED_BLOBS/(cancun.MAX_BLOBS_PER_BLOCK-cancun.TARGET_BLOBS_PER_BLOCK) + 1,
-	//			BlobsPerTransaction:           cancun.MAX_BLOBS_PER_BLOCK,
-	//			BlobTransactionMaxBlobGasCost: big.NewInt(1000000000),
-	//		},
-	//
-	//		// Next payloads will have max data blobs each
-	//		NewPayloads{
-	//			PayloadCount:              DATA_GAS_COST_INCREMENT_EXCEED_BLOBS / (cancun.MAX_BLOBS_PER_BLOCK - cancun.TARGET_BLOBS_PER_BLOCK),
-	//			ExpectedIncludedBlobCount: cancun.MAX_BLOBS_PER_BLOCK,
-	//		},
-	//
-	//		// But there will be an empty payload, since the data gas cost increased
-	//		// and the last blob transaction was not included.
-	//		NewPayloads{
-	//			ExpectedIncludedBlobCount: 0,
-	//		},
-	//
-	//		// But it will be included in the next payload
-	//		NewPayloads{
-	//			ExpectedIncludedBlobCount: cancun.MAX_BLOBS_PER_BLOCK,
-	//		},
-	//	},
-	//},
+		BaseSpec: test.BaseSpec{
+			Name: "Blob Transactions On Block 1, Shanghai Genesis",
+			About: `
+			Tests the Cancun fork since Block 1.
+	
+			Verifications performed:
+			- Correct implementation of Engine API changes for Cancun:
+			  - engine_newPayloadV3, engine_forkchoiceUpdatedV3, engine_getPayloadV3
+			- Correct implementation of EIP-4844:
+			  - Blob transaction ordering and inclusion
+			  - Blob transaction blob gas cost checks
+			  - Verify Blob bundle on built payload
+			- Eth RPC changes for Cancun:
+			  - Blob fields in eth_getBlockByNumber
+			  - Beacon root in eth_getBlockByNumber
+			  - Blob fields in transaction receipts from eth_getTransactionReceipt
+			`,
+			MainFork: config.Cancun,
+			// We fork after genesis
+			ForkHeight: 1,
+		},
+
+		TestSequence: TestSequence{
+			// We are starting at Shanghai genesis so send a couple payloads to reach the fork
+			NewPayloads{},
+
+			// First, we send a couple of blob transactions on genesis,
+			// with enough data gas cost to make sure they are included in the first block.
+			SendBlobTransactions{
+				TransactionCount:              cancun.TARGET_BLOBS_PER_BLOCK,
+				BlobTransactionMaxBlobGasCost: big.NewInt(1000000000),
+			},
+
+			// We create the first payload, and verify that the blob transactions
+			// are included in the payload.
+			// We also verify that the blob transactions are included in the blobs bundle.
+			NewPayloads{
+				ExpectedIncludedBlobCount: cancun.TARGET_BLOBS_PER_BLOCK,
+				ExpectedBlobs:             helper.GetBlobList(0, cancun.TARGET_BLOBS_PER_BLOCK),
+			},
+
+			// Try to increase the data gas cost of the blob transactions
+			// by maxing out the number of blobs for the next payloads.
+			SendBlobTransactions{
+				TransactionCount:              DATA_GAS_COST_INCREMENT_EXCEED_BLOBS/(cancun.MAX_BLOBS_PER_BLOCK-cancun.TARGET_BLOBS_PER_BLOCK) + 1,
+				BlobsPerTransaction:           cancun.MAX_BLOBS_PER_BLOCK,
+				BlobTransactionMaxBlobGasCost: big.NewInt(1000000000),
+			},
+
+			// Next payloads will have max data blobs each
+			NewPayloads{
+				PayloadCount:              DATA_GAS_COST_INCREMENT_EXCEED_BLOBS / (cancun.MAX_BLOBS_PER_BLOCK - cancun.TARGET_BLOBS_PER_BLOCK),
+				ExpectedIncludedBlobCount: cancun.MAX_BLOBS_PER_BLOCK,
+			},
+
+			// But there will be an empty payload, since the data gas cost increased
+			// and the last blob transaction was not included.
+			NewPayloads{
+				ExpectedIncludedBlobCount: 0,
+			},
+
+			// But it will be included in the next payload
+			NewPayloads{
+				ExpectedIncludedBlobCount: cancun.MAX_BLOBS_PER_BLOCK,
+			},
+		},
+	},
+
+	&CancunBaseSpec{
+
+		BaseSpec: test.BaseSpec{
+			Name: "Blob Transactions On Block 1, Cancun Genesis",
+			About: `
+			Tests the Cancun fork since genesis.
+	
+			Verifications performed:
+			* See Blob Transactions On Block 1, Shanghai Genesis
+			`,
+			MainFork: config.Cancun,
+		},
+
+		TestSequence: TestSequence{
+			NewPayloads{}, // Create a single empty payload to push the client through the fork.
+			// First, we send a couple of blob transactions on genesis,
+			// with enough data gas cost to make sure they are included in the first block.
+			SendBlobTransactions{
+				TransactionCount:              cancun.TARGET_BLOBS_PER_BLOCK,
+				BlobTransactionMaxBlobGasCost: big.NewInt(1000000000),
+			},
+
+			// We create the first payload, and verify that the blob transactions
+			// are included in the payload.
+			// We also verify that the blob transactions are included in the blobs bundle.
+			NewPayloads{
+				ExpectedIncludedBlobCount: cancun.TARGET_BLOBS_PER_BLOCK,
+				ExpectedBlobs:             helper.GetBlobList(0, cancun.TARGET_BLOBS_PER_BLOCK),
+			},
+
+			// Try to increase the data gas cost of the blob transactions
+			// by maxing out the number of blobs for the next payloads.
+			SendBlobTransactions{
+				TransactionCount:              DATA_GAS_COST_INCREMENT_EXCEED_BLOBS/(cancun.MAX_BLOBS_PER_BLOCK-cancun.TARGET_BLOBS_PER_BLOCK) + 1,
+				BlobsPerTransaction:           cancun.MAX_BLOBS_PER_BLOCK,
+				BlobTransactionMaxBlobGasCost: big.NewInt(1000000000),
+			},
+
+			// Next payloads will have max data blobs each
+			NewPayloads{
+				PayloadCount:              DATA_GAS_COST_INCREMENT_EXCEED_BLOBS / (cancun.MAX_BLOBS_PER_BLOCK - cancun.TARGET_BLOBS_PER_BLOCK),
+				ExpectedIncludedBlobCount: cancun.MAX_BLOBS_PER_BLOCK,
+			},
+
+			// But there will be an empty payload, since the data gas cost increased
+			// and the last blob transaction was not included.
+			NewPayloads{
+				ExpectedIncludedBlobCount: 0,
+			},
+
+			// But it will be included in the next payload
+			NewPayloads{
+				ExpectedIncludedBlobCount: cancun.MAX_BLOBS_PER_BLOCK,
+			},
+		},
+	},
 	&CancunBaseSpec{
 
 		BaseSpec: test.BaseSpec{
@@ -185,7 +185,7 @@ var Tests = []test.Spec{
 			SendBlobTransactions{
 				TransactionCount:              cancun.MAX_BLOBS_PER_BLOCK + 1,
 				BlobsPerTransaction:           1,
-				BlobTransactionMaxBlobGasCost: big.NewInt(500000000),
+				BlobTransactionMaxBlobGasCost: big.NewInt(00000000),
 			},
 
 			// First four payloads have cancun.MAX_BLOBS_PER_BLOCK-1 blobs each
