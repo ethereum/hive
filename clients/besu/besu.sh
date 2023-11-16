0e82f3ffa5486b3f38e28b5aef82f2b1aba7a940
Mario Vega <marioevz@gmail.com>
2023-11-16 13:37:25 +0000

    clients/besu: Remove trusted setup

--- clients/besu/besu.sh
+++ clients/besu/besu.sh
@@ -159,11 +159,6 @@ if [ "$HIVE_TERMINAL_TOTAL_DIFFICULTY" != "" ]; then
     RPCFLAGS="$RPCFLAGS --engine-host-allowlist=* --engine-jwt-enabled --engine-jwt-secret /jwtsecret"
 fi
 
-# Enable KZG trusted setup if cancun timestamp is set, needed for custom genesis on Besu with Cancun
-if [ "$HIVE_CANCUN_TIMESTAMP" != "" ]; then
-    FLAGS="$FLAGS --kzg-trusted-setup=/trusted_setup.txt"
-fi
-
 # Start Besu.
 if [ -z "$HAS_IMPORT" ]; then
     cmd="$besu $FLAGS $RPCFLAGS"
