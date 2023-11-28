f931b46287a8ebc7124ae33ab830b3d1322d475a
Mario Vega <marioevz@gmail.com>
2023-11-28 20:22:22 +0000

    hivesim/docs: cleanup

--- hivesim/docs.go
+++ hivesim/docs.go
@@ -13,6 +13,7 @@ import (
 	"gopkg.in/inconshreveable/log15.v2"
 )
 
+// Converts a string to a valid markdown link.
 func toMarkdownLink(title string) string {
 	removeChars := []string{":", "#", "'", "\"", "`", "*", "+", ",", ";"}
 	title = strings.ReplaceAll(strings.ToLower(title), " ", "-")
@@ -22,6 +23,7 @@ func toMarkdownLink(title string) string {
 	return title
 }
 
+// Formats the description string to be printed in the markdown file.
 func formatDescription(desc string) string {
 	desc = dedent.Dedent(desc)
 	// Replace single quotes with backticks, since backticks are used for markdown code blocks and
@@ -30,16 +32,20 @@ func formatDescription(desc string) string {
 	return desc
 }
 
+// Represents a single test case to be printed in the markdown file.
 type markdownTestCase simapi.TestRequest
 
+// Returns the command-line to run the test case.
 func (tc *markdownTestCase) commandLine(simName string, suiteName string) string {
 	return fmt.Sprintf("./hive --client <CLIENTS> --sim %s --sim.limit \"%s/%s\"", simName, suiteName, tc.Name)
 }
 
+// Returns true if the test case should be printed in the markdown file.
 func (tc *markdownTestCase) toBePrinted() bool {
 	return tc.Description != ""
 }
 
+// Returns the test case display name.
 func (tc *markdownTestCase) displayName() string {
 	if tc.DisplayName != "" {
 		return tc.DisplayName
@@ -47,10 +53,14 @@ func (tc *markdownTestCase) displayName() string {
 	return tc.Name
 }
 
+// Returns the test case description.
 func (tc *markdownTestCase) description() string {
 	return formatDescription(tc.Description)
 }
 
+// Returns the test case markdown representation.
+// Requires the simulation name and the suite name.
+// The depth parameter is used to determine the number of '#' to use for the test case title.
 func (tc *markdownTestCase) toMarkdown(simName string, suiteName string, depth int) string {
 	sb := strings.Builder{}
 
@@ -71,12 +81,14 @@ func (tc *markdownTestCase) toMarkdown(simName string, suiteName string, depth i
 	return sb.String()
 }
 
+// Represents a test suite to be printed in the markdown file.
 type markdownSuite struct {
 	simapi.TestRequest
 	running bool
 	tests   map[TestID]*markdownTestCase
 }
 
+// Returns true if the test suite should be printed in the markdown file.
 func (s *markdownSuite) displayName() string {
 	if s.DisplayName != "" {
 		return s.DisplayName
@@ -84,6 +96,10 @@ func (s *markdownSuite) displayName() string {
 	return fmt.Sprintf("`%s`", s.Name)
 }
 
+// Returns the markdown file path for the test suite.
+// If the location is set, it will be used as the directory path, and the filename will be
+// `TESTS.md`.
+// Otherwise, the filename will be `TESTS-<SUITE_NAME>.md`.
 func (s *markdownSuite) mardownFilePath() string {
 	if s.Location != "" {
 		return fmt.Sprintf("%s/TESTS.md", s.Location)
@@ -92,16 +108,18 @@ func (s *markdownSuite) mardownFilePath() string {
 	return fmt.Sprintf("TESTS-%s.md", title)
 }
 
+// Returns the command-line to run the test suite.
 func (s *markdownSuite) commandLine(simName string) string {
 	return fmt.Sprintf("./hive --client <CLIENTS> --sim %s --sim.limit \"%s/\"", simName, s.Name)
 }
 
+// Returns true if the test suite should be printed in the markdown file.
 func (s *markdownSuite) description() string {
 	return formatDescription(s.Description)
 }
 
+// Returns a sorted list of the test IDs
 func (s *markdownSuite) testIDs() []TestID {
-	// Returns a sorted list of the test IDs
 	testIDs := make([]TestID, 0, len(s.tests))
 	for testID := range s.tests {
 		testIDs = append(testIDs, testID)
@@ -112,6 +130,7 @@ func (s *markdownSuite) testIDs() []TestID {
 	return testIDs
 }
 
+// Returns a list of all the unique categories in all of the test cases.
 func (s *markdownSuite) getCategories() []string {
 	categoryMap := make(map[string]struct{})
 	categories := make([]string, 0)
@@ -127,6 +146,7 @@ func (s *markdownSuite) getCategories() []string {
 	return categories
 }
 
+// Returns the markdown representation of the test suite.
 func (s *markdownSuite) toMarkdown(simName string) (string, error) {
 	headerBuilder := strings.Builder{}
 	categoryBuilder := map[string]*strings.Builder{}
@@ -190,6 +210,7 @@ func (s *markdownSuite) toMarkdown(simName string) (string, error) {
 	return headerBuilder.String(), nil
 }
 
+// Writes the markdown representation of the test suite to a file.
 func (s *markdownSuite) toMarkdownFile(fw FileWriter, simName string) error {
 	// Create the file.
 	file, err := fw.CreateWriter(s.mardownFilePath())
@@ -207,13 +228,16 @@ func (s *markdownSuite) toMarkdownFile(fw FileWriter, simName string) error {
 	return err
 }
 
-// Docs collector object
+// Docs collector object:
+// - Collects the test cases and test suites.
+// - Generates markdown files.
 type docsCollector struct {
 	simName   string
 	outputDir string
 	suites    map[SuiteID]*markdownSuite
 }
 
+// Returns the simulator name from the path of the currently running binary.
 func simulatorNameFromBinaryPath() string {
 	execPath, err := os.Executable()
 	if err != nil {
@@ -233,6 +257,7 @@ func simulatorNameFromBinaryPath() string {
 }
 
 // NewDocsCollector creates a new docs collector object.
+// Tries to parse the simulator name and also the output directory from the environment variables.
 func NewDocsCollector() *docsCollector {
 	docs := &docsCollector{
 		suites: make(map[SuiteID]*markdownSuite),
@@ -268,6 +293,7 @@ func (docs *docsCollector) AnyRunning() bool {
 	return false
 }
 
+// Returns a sorted list of the suite IDs
 func (docs *docsCollector) suiteIDs() []SuiteID {
 	// Returns a sorted list of the suite IDs
 	suiteIDs := make([]SuiteID, 0, len(docs.suites))
@@ -280,6 +306,7 @@ func (docs *docsCollector) suiteIDs() []SuiteID {
 	return suiteIDs
 }
 
+// Starts a new test suite, and appends it to the suites map.
 func (docs *docsCollector) StartSuite(suite *simapi.TestRequest, simlog string) (SuiteID, error) {
 	// Create a new markdown suite.
 	markdownSuite := &markdownSuite{
@@ -295,6 +322,8 @@ func (docs *docsCollector) StartSuite(suite *simapi.TestRequest, simlog string) 
 	return suiteID, nil
 }
 
+// Ends a test suite. If the suite does not exist, returns an error.
+// If all suites are done, generates the markdown files.
 func (docs *docsCollector) EndSuite(testSuite SuiteID) error {
 	suite, ok := docs.suites[testSuite]
 	if !ok {
@@ -310,6 +339,8 @@ func (docs *docsCollector) EndSuite(testSuite SuiteID) error {
 	return nil
 }
 
+// Starts a new test case, and appends it to the tests map in the test suite.
+// If the suite does not exist, returns an error.
 func (docs *docsCollector) StartTest(testSuite SuiteID, test *simapi.TestRequest) (TestID, error) {
 	// Create a new markdown test case.
 	markdownTest := markdownTestCase(*test)
@@ -325,13 +356,21 @@ func (docs *docsCollector) StartTest(testSuite SuiteID, test *simapi.TestRequest
 	return testID, nil
 }
 
+// Ends a test case. If the suite or the test case or suite do not exist, returns an error.
 func (docs *docsCollector) EndTest(testSuite SuiteID, test TestID, testResult TestResult) error {
-	// No-op in docs mode.
+	// Check if suite exists.
+	if _, ok := docs.suites[testSuite]; !ok {
+		return fmt.Errorf("test suite %d does not exist", testSuite)
+	}
+	// Check if test exists.
+	if _, ok := docs.suites[testSuite].tests[test]; !ok {
+		return fmt.Errorf("test %d does not exist", test)
+	}
 	return nil
 }
 
+// Return a generic "Client" client type.
 func (docs *docsCollector) ClientTypes() ([]*ClientDefinition, error) {
-	// Return a dummy "docs" client type.
 	return []*ClientDefinition{
 		{
 			Name:    "Client",
@@ -340,6 +379,7 @@ func (docs *docsCollector) ClientTypes() ([]*ClientDefinition, error) {
 	}, nil
 }
 
+// Generates the markdown index file that will point to the test suites' markdown files.
 func (docs *docsCollector) generateIndex(simName string) (string, error) {
 	headerBuilder := strings.Builder{}
 	headerBuilder.WriteString(fmt.Sprintf("# Simulator `%s` Test Cases\n\n", simName))
@@ -353,6 +393,7 @@ func (docs *docsCollector) generateIndex(simName string) (string, error) {
 	return headerBuilder.String() + "\n", nil
 }
 
+// Generates the markdown index file that will point to the test suites' markdown files.
 func (docs *docsCollector) generateIndexFile(fw FileWriter, simName string) error {
 	markdownIndex, err := docs.generateIndex(simName)
 	if err != nil {
@@ -367,28 +408,7 @@ func (docs *docsCollector) generateIndexFile(fw FileWriter, simName string) erro
 	return err
 }
 
-type FileWriter interface {
-	CreateWriter(path string) (io.WriteCloser, error)
-}
-
-type fileWriter struct {
-	path string
-}
-
-var _ FileWriter = (*fileWriter)(nil)
-
-func (fw *fileWriter) CreateWriter(path string) (io.WriteCloser, error) {
-	filePath := filepath.FromSlash(fmt.Sprintf("%s/%s", fw.path, path))
-	if err := os.MkdirAll(filepath.Dir(filePath), 0755); err != nil {
-		return nil, err
-	}
-	return os.Create(filePath)
-}
-
-func NewFileWriter(path string) FileWriter {
-	return &fileWriter{path}
-}
-
+// Generates the markdown files for all the test suites, including the index file.
 func (docs *docsCollector) genSimulatorMarkdownFiles(fw FileWriter) error {
 	err := docs.generateIndexFile(fw, docs.simName)
 	if err != nil {
@@ -401,4 +421,30 @@ func (docs *docsCollector) genSimulatorMarkdownFiles(fw FileWriter) error {
 		}
 	}
 	return nil
+}
+
+// FileWriter interface. Used to create the markdown files.
+type FileWriter interface {
+	CreateWriter(path string) (io.WriteCloser, error)
+}
+
+// FileWriter implementation that writes to the filesystem.
+// The basePath is the root directory where the files will be created.
+type fileWriter struct {
+	basePath string
+}
+
+// Creates a new file writer. The path starts from the base directory that is the fileWriter.
+// Subdirectories will be created if they do not exist.
+func (fw fileWriter) CreateWriter(path string) (io.WriteCloser, error) {
+	filePath := filepath.FromSlash(fmt.Sprintf("%s/%s", filepath.ToSlash(fw.basePath), path))
+	if err := os.MkdirAll(filepath.Dir(filePath), 0755); err != nil {
+		return nil, err
+	}
+	return os.Create(filePath)
+}
+
+// NewFileWriter creates a new file writer with the given base path.
+func NewFileWriter(basePath string) FileWriter {
+	return fileWriter{basePath}
 }
