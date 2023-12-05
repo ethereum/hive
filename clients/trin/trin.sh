288cd95702e9448316aa3de06620e26df38f3f97
Kolby Moroz Liebl <31669092+KolbyML@users.noreply.github.com>
2023-12-05 13:16:51 -0700

    chore: small fixes

--- clients/trin/trin.sh
+++ clients/trin/trin.sh
@@ -7,7 +7,7 @@ IP_ADDR=$(hostname -i | awk '{print $1}')
 
 # if HIVE_CLIENT_PRIVATE_KEY isn't set or doesn't exist do y, else do z
 if [ -z ${HIVE_CLIENT_PRIVATE_KEY+x} ]; then
-  RUST_LOG=debug trin --web3-transport http --web3-http-address http://0.0.0.0:8545 --external-address "$IP_ADDR":9000 --bootnodes none
+  RUST_LOG=debug trin --web3-transport http --web3-http-address http://0.0.0.0:8545 --external-address "$IP_ADDR":9009 --bootnodes none
 else
-  RUST_LOG=debug trin --web3-transport http --web3-http-address http://0.0.0.0:8545 --external-address "$IP_ADDR":9000 --bootnodes none --unsafe-private-key 0x${HIVE_CLIENT_PRIVATE_KEY}
+  RUST_LOG=debug trin --web3-transport http --web3-http-address http://0.0.0.0:8545 --external-address "$IP_ADDR":9009 --bootnodes none --unsafe-private-key 0x${HIVE_CLIENT_PRIVATE_KEY}
 fi
