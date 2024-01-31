a275e9b9d104d2080416c0a2e365033b900c1fe6
Kolby Moroz Liebl <31669092+KolbyML@users.noreply.github.com>
2024-01-30 18:14:37 -0700

    clients: Add portal network client definitions

---
+++ clients/trin/trin.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Immediately abort the script on any error encountered
+set -e
+
+IP_ADDR=$(hostname -i | awk '{print $1}')
+FLAGS=""
+
+if [ "$HIVE_CLIENT_PRIVATE_KEY" != "" ]; then
+    FLAGS="$FLAGS --unsafe-private-key 0x$HIVE_CLIENT_PRIVATE_KEY"
+fi
+
+if [ "$HIVE_PORTAL_NETWORKS_SELECTED" != "" ]; then
+    FLAGS="$FLAGS --networks $HIVE_PORTAL_NETWORKS_SELECTED"
+else
+    FLAGS="$FLAGS --networks history"
+fi
+
+RUST_LOG=debug trin --web3-transport http --web3-http-address http://0.0.0.0:8545 --external-address "$IP_ADDR":9009 --bootnodes none $FLAGS
---
+++ clients/ultralight/ultralight.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Immediately abort the script on any error encountered
+set -e
+
+IP_ADDR=$(hostname -i | awk '{print $1}')
+FLAGS=""
+
+if [ "$HIVE_CLIENT_PRIVATE_KEY" != "" ]; then
+    FLAGS="$FLAGS --pk=0x1a2408021220$HIVE_CLIENT_PRIVATE_KEY"
+fi
+
+if [ "$HIVE_PORTAL_NETWORKS_SELECTED" != "" ]; then
+    FLAGS="$FLAGS --networks=$HIVE_PORTAL_NETWORKS_SELECTED"
+else
+    FLAGS="$FLAGS --networks=history"
+fi
+
+DEBUG=* node /ultralight/packages/cli/dist/index.js --bindAddress="$IP_ADDR:9000" --dataDir="./data" --rpcPort=8545 $FLAGS
