236250c04be45c0061ab4cc7d612b0e097299a52
Kolby Moroz Liebl <31669092+KolbyML@users.noreply.github.com>
2024-01-31 04:47:45 -0700

    clients: add portal network client definitions (#977)

---
+++ clients/fluffy/fluffy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Immediately abort the script on any error encountered
+set -e
+
+IP_ADDR=$(hostname -i | awk '{print $1}')
+FLAGS=""
+
+if [ "$HIVE_PORTAL_NETWORKS_SELECTED" != "" ]; then
+    if [[ $HIVE_PORTAL_NETWORKS_SELECTED =~ "beacon" ]]; then
+        # Providing atrusted block root is required currently to enable the beacon network.
+        # It can be a made up value for now as tests are not doing any sync.
+        FLAGS="$FLAGS --trusted-block-root:0x0000000000000000000000000000000000000000000000000000000000000000"
+    fi
+fi
+
+
+if [ "$HIVE_CLIENT_PRIVATE_KEY" != "" ]; then
+    FLAGS="$FLAGS --netkey-unsafe=0x$HIVE_CLIENT_PRIVATE_KEY"
+fi
+
+# Fluffy runs all networks by default, so we can not configure to run networks individually
+fluffy --rpc --rpc-address="0.0.0.0" --nat:extip:"$IP_ADDR" --network=none --log-level="debug" $FLAGS
---
+++ clients/trin/trin_version.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Immediately abort the script on any error encountered
+set -e
+
+#trin --version | tail -1 | sed "s/ /\//g"
+echo "latest"
