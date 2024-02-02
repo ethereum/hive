e1a265ee042d3ec5f68078888bd1152a3395d864
Kolby Moroz Liebl <31669092+KolbyML@users.noreply.github.com>
2024-02-01 22:06:23 -0700

    simulators/portal/history/trin-bridge: add portal network history trin-bridge simulator

---
+++ simulators/portal/history/trin-bridge/src/constants.rs
@@ -0,0 +1,4 @@
+pub const TRIN_BRIDGE_CLIENT_TYPE: &str = "trin-bridge";
+pub const BOOTNODES_ENVIRONMENT_VARIABLE: &str = "HIVE_BOOTNODES";
+pub const HIVE_CHECK_LIVE_PORT: &str = "HIVE_CHECK_LIVE_PORT";
+pub const TEST_DATA_FILE_PATH: &str = "./test-data/test_data_collection_of_forks_blocks.yaml";
