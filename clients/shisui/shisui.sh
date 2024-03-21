fc48f9fb6e45bbef7ce1092513802b46bae556a3
fearlessfe <505380967@qq.com>
2024-03-21 23:16:17 +0800

    feat: update startup script

---
+++ clients/shisui/shisui.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Immediately abort the script on any error encountered
+set -e
+
+FLAGS=""
+
+if [ "$HIVE_LOGLEVEL" != "" ]; then
+    FLAGS="$FLAGS --loglevel $HIVE_LOGLEVEL"
+fi
+
+if [ "$HIVE_CLIENT_PRIVATE_KEY" != "" ]; then
+    FLAGS="$FLAGS --private.key 0x$HIVE_CLIENT_PRIVATE_KEY"
+fi
+
+if [ "$HIVE_PORTAL_NETWORKS_SELECTED" != "" ]; then
+    FLAGS="$FLAGS --networks $HIVE_PORTAL_NETWORKS_SELECTED"
+else
+    FLAGS="$FLAGS --networks history"
+fi
+
+if [ "$HIVE_BOOTNODE" != "" ]; then
+    FLAGS="$FLAGS --bootnodes=$HIVE_BOOTNODE"
+fi
+
+app $FLAGS
