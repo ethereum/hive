d61b11e51815a82937f7683dd7f1d25132673983
Chen Kai <281165273grape@gmail.com>
2024-06-14 08:19:20 +0800

    clients/shisui: enable nat in hive test (#1121)
    * feat:enable nat in hive test
    
    Signed-off-by: Chen Kai <281165273grape@gmail.com>
    
    * Update clients/shisui/shisui.sh
    
    Co-authored-by: Kolby Moroz Liebl <31669092+KolbyML@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Chen Kai <281165273grape@gmail.com>
    Co-authored-by: Kolby Moroz Liebl <31669092+KolbyML@users.noreply.github.com>

--- clients/shisui/shisui.sh
+++ clients/shisui/shisui.sh
@@ -4,6 +4,7 @@
 set -e
 
 FLAGS=""
+IP_ADDR=$(hostname -i | awk '{print $1}')
 
 if [ "$HIVE_LOGLEVEL" != "" ]; then
     FLAGS="$FLAGS --loglevel $HIVE_LOGLEVEL"
@@ -22,6 +23,8 @@ fi
 if [ "$HIVE_BOOTNODE" != "" ]; then
     FLAGS="$FLAGS --bootnodes=$HIVE_BOOTNODE"
 fi
+
+FLAGS="$FLAGS --nat extip:$IP_ADDR"
 
 app $FLAGS
 
