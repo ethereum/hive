4ca1936688f32eae23072ef7620b2223bd06e817
Leandro Serra <leandro.serra@lambdaclass.com>
2025-03-06 16:37:03 -0300

    modify ethrex.sh to use new import subcommand syntax

--- clients/ethrex/ethrex.sh
+++ clients/ethrex/ethrex.sh
@@ -59,7 +59,7 @@ set +ex
 echo "Loading initial blockchain..."
 if [ -f /chain.rlp ]; then
     echo "Importing chain.rlp..."
-    $ethrex import --network /genesis.json /chain.rlp
+    $ethrex $FLAGS $HIVE_ETHREX_FLAGS import /chain.rlp
 else
     echo "Warning: chain.rlp not found."
 fi
@@ -67,7 +67,7 @@ fi
 # Load the remainder of the test chain
 if [ -d /blocks ]; then
     echo "Loading remaining individual blocks..."
-    $ethrex import --network /genesis.json /blocks
+    $ethrex $FLAGS $HIVE_ETHREX_FLAGS import /blocks
 else
     echo "Warning: blocks folder not found."
 fi
