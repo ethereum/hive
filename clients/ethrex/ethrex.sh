c6151db689af5dc5c7a10e2ab424d7bb171561f1
fedacking <francisco.gauna@lambdaclass.com>
2025-05-28 08:55:05 -0300

    clients/ethrex: fixed ethrex tests failing due to premature exit (#1293)

--- clients/ethrex/ethrex.sh
+++ clients/ethrex/ethrex.sh
@@ -85,13 +85,14 @@ fi
 # Configure RPC.
 FLAGS="$FLAGS --http.addr=0.0.0.0  --authrpc.addr=0.0.0.0"
 
-# We don't support pre merge so we exit with an error if HIVE_TERMINAL_TOTAL_DIFFICULTY is set
+# We don't support pre merge
 if [ "$HIVE_TERMINAL_TOTAL_DIFFICULTY" != "" ]; then
     JWT_SECRET="7365637265747365637265747365637265747365637265747365637265747365"
     echo -n $JWT_SECRET > /jwt.secret
     FLAGS="$FLAGS  --authrpc.jwtsecret=/jwt.secret"
 else
-    exit 1
+    # We dont exit because some tests require this
+    echo "Warning: HIVE_TERMINAL_TOTAL_DIFFICULTY not supported."
 fi
 
 FLAGS="$FLAGS  $HIVE_ETHREX_FLAGS"
