8d692b48ee421100aa3d4ed2d622f75079d6602c
Felix Lange <fjl@twurst.com>
2025-10-23 16:54:18 +0200

    simulators/ethereum/sync: update chain with PoS from genesis

--- simulators/ethereum/sync/mkchain.sh
+++ simulators/ethereum/sync/mkchain.sh
@@ -4,9 +4,11 @@ wd="$(pwd)"
 cd ../../..
 go build ./cmd/hivechain
 ./hivechain generate \
-    -outdir "$wd/chain" \
-    -length 2000 \
-    -lastfork shanghai \
+    -pos \
+    -length 3000 \
     -tx-interval 5 \
     -fork-interval 0 \
+    -finalized-distance 50 \
+    -outdir "$wd/chain" \
+    -lastfork cancun \
     -outputs forkenv,genesis,chain,headblock,headfcu,headnewpayload
