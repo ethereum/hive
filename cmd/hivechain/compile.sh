50e3006439d51aa00702d3d43d458e2520875317
Felix Lange <fjl@twurst.com>
2025-10-24 16:48:23 +0200

    cmd/hivechain: run geas as tool

--- cmd/hivechain/compile.sh
+++ cmd/hivechain/compile.sh
@@ -5,5 +5,5 @@ set -e
 for contract in contracts/*.eas; do
     output=bytecode/$(basename ${contract%.*}).bin
     echo $contract "->" $output
-    geas -a -bin -o $output $contract
+    go tool geas -a -bin -o $output $contract
 done
