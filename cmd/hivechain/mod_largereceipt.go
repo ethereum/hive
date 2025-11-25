1eac96d2a6be6c4bd3d4c2d500444c19976b02a9
Felix Lange <fjl@twurst.com>
2025-11-25 14:15:15 +0100

    cmd/hivechain: add comments in modLargeReceipt

--- cmd/hivechain/mod_largereceipt.go
+++ cmd/hivechain/mod_largereceipt.go
@@ -7,6 +7,7 @@ import (
 
 func init() {
 	register("tx-largereceipt", func() blockModifier {
+		// Gas parameters here are calculated to create a block with > 10MB of receipts.
 		const logCost = 1607055
 		const pushCost = 10
 		return &modLargeReceipt{
@@ -16,41 +17,37 @@ func init() {
 	})
 }
 
+// modLargeReceipt creates blocks with large receipts. It emits multiple transactions
+// within a single block, where each transaction has a single log with large data.
 type modLargeReceipt struct {
-	run      bool
-	gasLimit uint64
-	txCount  uint64
-	block    uint64
+	didRun   bool
+	gasLimit uint64 // gas of single transaction
+	txCount  uint64 // number of transactions added to block
+	block    uint64 // block number where txs were included
 }
 
 func (m *modLargeReceipt) apply(ctx *genBlockContext) bool {
-	if m.run {
-		return false
-	}
-	if !ctx.HasGas(m.gasLimit * m.txCount) {
+	if m.didRun || !ctx.HasGas(m.gasLimit*m.txCount) {
 		return false
 	}
 
 	sender := ctx.TxSenderAccount()
-
 	for range m.txCount {
-		var txdata = &types.LegacyTx{
+		ctx.AddNewTx(sender, &types.LegacyTx{
 			Nonce:    ctx.AccountNonce(sender.addr),
 			Gas:      m.gasLimit,
 			GasPrice: ctx.TxGasFeeCap(),
 			Data:     modLargeReceiptCode,
-		}
-
-		ctx.AddNewTx(sender, txdata)
+		})
 	}
-	m.run = true
 	m.block = ctx.NumberU64()
-
+	m.didRun = true
 	return true
 }
 
+// txInfo is just the block number that has the receipts.
 func (m *modLargeReceipt) txInfo() any {
-	if m.run {
+	if m.didRun {
 		return m.block
 	}
 	return nil
