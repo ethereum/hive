8aa325379343602b4f4274755e2beca03a4a135b
Chase Wright <chase.wright@ethereum.org>
2026-02-25 13:32:01 -0600

    Add testing namespace to clients

--- clients/go-ethereum/geth.sh
+++ clients/go-ethereum/geth.sh
@@ -138,13 +138,8 @@ if [ "$HIVE_MINER_EXTRA" != "" ]; then
 fi
 
 # Configure RPC.
-FLAGS="$FLAGS --http --http.addr=0.0.0.0 --http.port=8545 --http.api=admin,debug,eth,miner,net,txpool,web3"
-FLAGS="$FLAGS --ws --ws.addr=0.0.0.0 --ws.origins \"*\" --ws.api=admin,debug,eth,miner,net,txpool,web3"
-
-if [ "$HIVE_TERMINAL_TOTAL_DIFFICULTY" != "" ]; then
-    echo "0x7365637265747365637265747365637265747365637265747365637265747365" > /jwtsecret
-    FLAGS="$FLAGS --authrpc.addr=0.0.0.0 --authrpc.port=8551 --authrpc.jwtsecret /jwtsecret"
-fi
+FLAGS="$FLAGS --http --http.addr=0.0.0.0 --http.port=8545 --http.api=admin,debug,eth,miner,net,txpool,web3,testing"
+FLAGS="$FLAGS --ws --ws.addr=0.0.0.0 --ws.origins \"*\" --ws.api=admin,debug,eth,miner,net,txpool,web3,testing"
 
 # Configure GraphQL.
 if [ "$HIVE_GRAPHQL_ENABLED" != "" ]; then
