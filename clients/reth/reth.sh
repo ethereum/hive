24d117c086becbdb162c8d99841928c7bb1b136f
Emma Jamieson-Hoare <emmajam@users.noreply.github.com>
2026-05-22 07:14:39 +0100

    fix(reth): add flag for startup to mark idle (#1503)

--- clients/reth/reth.sh
+++ clients/reth/reth.sh
@@ -163,7 +163,7 @@ if [ "$HIVE_TERMINAL_TOTAL_DIFFICULTY" != "" ]; then
 fi
 
 # Configure NAT and disable pruning
-FLAGS="$FLAGS --nat none --block-interval 500000"
+FLAGS="$FLAGS --nat none --block-interval 500000 --debug.startup-sync-state-idle"
 
 if [ "$HIVE_DISCV5" != "" ]; then
     FLAGS="$FLAGS --discovery.v5.port=30303"
