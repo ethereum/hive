08405876c3f1f6630010d57806365ea64b0630d5
Derek Sorken <113465018+Dsorken@users.noreply.github.com>
2026-06-20 01:51:43 -0600

    hivesim-rs, simulators: add devnet to run digest (#1553)
    add devnet digest

--- hivesim-rs/src/lib.rs
+++ hivesim-rs/src/lib.rs
@@ -9,7 +9,7 @@ pub mod utils;
 
 pub use simulation::Simulation;
 pub use testapi::{
-    run_suite, Client, NClientTestSpec, PlannedTestSpec, SharedClientScenario,
-    SharedClientTestSpec, Suite, Test, TestSpec,
+    run_suite, run_suite_with_plan_metadata, Client, NClientTestSpec, PlannedTestSpec,
+    SharedClientScenario, SharedClientTestSpec, Suite, Test, TestSpec,
 };
 pub use testmatch::TestMatcher;
