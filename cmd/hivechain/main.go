ethereum/hive

commit 0c044053b98b9a9c8bbc26a86ff249a67182d6a6

cmd/hivechain: support for newer forks, more chain outputs (#909)

cmd/hivechain/main.go
@@ -24,34 +24,75 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 
 	"github.com/ethereum/go-ethereum/core/types"
 	ethlog "github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/rlp"
 )
 
-const usage = "Usage: hivechain generate|print|print-genesis|trim [ options ] ..."
-
 func main() {
 	// Initialize go-ethereum logging.
 	// This is mostly for displaying the DAG generator progress.
 	handler := ethlog.StreamHandler(os.Stderr, ethlog.TerminalFormat(false))
-	ethlog.Root().SetHandler(ethlog.LvlFilterHandler(ethlog.LvlInfo, handler))
+	ethlog.Root().SetHandler(ethlog.LvlFilterHandler(ethlog.LvlWarn, handler))
+
+	flag.Usage = usage
 
 	if len(os.Args) < 2 {
-		fatalf(usage)
+		flag.Usage()
+		os.Exit(1)
 	}
 	switch os.Args[1] {
 	case "generate":
 		generateCommand(os.Args[2:])
 	case "print":
 		printCommand(os.Args[2:])
-	case "print-genesis":
-		printGenesisCommand(os.Args[2:])
-	case "trim":
-		trimCommand(os.Args[2:])
 	default:
-		fatalf(usage)
+		flag.Usage()
+		os.Exit(1)
+	}
+}
+
+// generateCommand generates a test chain.
+func generateCommand(args []string) {
+	var (
+		cfg     generatorConfig
+		outlist = flag.String("outputs", "", "Enabled output modules")
+	)
+	flag.IntVar(&cfg.chainLength, "length", 2, "The length of the pow chain to generate")
+	flag.IntVar(&cfg.txInterval, "tx-interval", 10, "Add transactions to chain every n blocks")
+	flag.IntVar(&cfg.txCount, "tx-count", 1, "Maximum number of txs per block")
+	flag.IntVar(&cfg.forkInterval, "fork-interval", 0, "Number of blocks between fork activations")
+	flag.StringVar(&cfg.outputDir, "outdir", ".", "Destination directory")
+	flag.StringVar(&cfg.lastFork, "lastfork", "", "Name of the last fork to activate")
+	flag.CommandLine.Parse(args)
+
+	if *outlist != "" {
+		if *outlist == "all" {
+			cfg.outputs = outputFunctionNames()
+		}
+		cfg.outputs = splitAndTrim(*outlist)
+	}
+
+	cfg, err := cfg.withDefaults()
+	if err != nil {
+		panic(err)
+	}
+	g := newGenerator(cfg)
+	if err := g.run(); err != nil {
+		fatal(err)
+	}
+}
+
+func usage() {
+	o := flag.CommandLine.Output()
+	fmt.Fprintln(o, "Usage: hivechain generate|print [options...]")
+	flag.PrintDefaults()
+	fmt.Fprintln(o, "")
+	fmt.Fprintln(o, "List of available -outputs:")
+	for _, name := range outputFunctionNames() {
+		fmt.Fprintln(o, "  ", name)
 	}
 }
 
@@ -89,97 +130,15 @@ func printCommand(args []string) {
 	}
 }
 
-// printGenesisCommand displays the genesis post-state.
-func printGenesisCommand(args []string) {
-	flag.CommandLine.Parse(args)
-	if flag.NArg() != 1 {
-		fatalf("Usage: hivechain print-genesis <genesis.json>")
-	}
-
-	gspec, err := loadGenesis(flag.Arg(0))
-	if err != nil {
-		fatal(err)
-	}
-	block := gspec.ToBlock()
-	js, _ := json.MarshalIndent(block.Header(), "", "  ")
-	fmt.Println(string(js))
-}
-
-// trimCommand exports a subset of chain.rlp to a new file.
-func trimCommand(args []string) {
-	var (
-		from = flag.Uint("from", 0, "Start of block range to output")
-		to   = flag.Uint("to", 0, "End of block range to output (0 = all blocks)")
-	)
-	flag.CommandLine.Parse(args)
-	if flag.NArg() != 2 {
-		fatalf("Usage: hivechain trim [ options ] <chain.rlp> <newchain.rlp>")
-	}
-	if *to > 0 && *to <= *from {
-		fatalf("-to must be greater than -from")
-	}
-
-	input, err := os.Open(flag.Arg(0))
-	if err != nil {
-		fatal(err)
-	}
-	defer input.Close()
-
-	output, err := os.OpenFile(flag.Arg(1), os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
-	if err != nil {
-		fatal(err)
-	}
-	defer output.Close()
-
-	s := rlp.NewStream(bufio.NewReader(input), 0)
-	written := 0
-	for i := uint(0); ; i++ {
-		data, err := s.Raw()
-		if err == io.EOF {
-			break
-		} else if err != nil {
-			fatalf("block %d: %v", i, err)
-		}
-		if i >= *from {
-			if *to != 0 && i >= *to {
-				break
-			}
-			output.Write(data)
-			written++
+func splitAndTrim(s string) []string {
+	var list []string
+	for _, s := range strings.Split(s, ",") {
+		s = strings.TrimSpace(s)
+		if s != "" {
+			list = append(list, s)
 		}
 	}
-	fmt.Println(written, "blocks written to", flag.Arg(1))
-}
-
-// generateCommand generates a test chain.
-func generateCommand(args []string) {
-	var (
-		cfg     generatorConfig
-		genesis = flag.String("genesis", "", "The path and filename to the source genesis.json")
-		outdir  = flag.String("output", ".", "Chain destination folder")
-		pos     = flag.Bool("pos", false, "Enables PoS chain")
-	)
-	flag.IntVar(&cfg.blockCount, "length", 2, "The length of the pow chain to generate")
-	flag.IntVar(&cfg.posBlockCount, "poslength", 2, "The length of the pos chain to generate")
-	flag.IntVar(&cfg.blockTimeSec, "blocktime", 30, "The desired block time in seconds")
-	flag.IntVar(&cfg.txInterval, "tx-interval", 10, "Add transactions to chain every n blocks")
-	flag.IntVar(&cfg.txCount, "tx-count", 1, "Maximum number of txs per block")
-	flag.CommandLine.Parse(args)
-
-	if *genesis == "" {
-		fatalf("Missing -genesis option, please supply a genesis.json file.")
-	}
-	cfg.isPoS = *pos
-
-	gspec, err := loadGenesis(*genesis)
-	if err != nil {
-		fatal(err)
-	}
-	cfg.genesis = *gspec
-
-	if err := cfg.writeTestChain(*outdir); err != nil {
-		fatal(err)
-	}
+	return list
 }
 
 func fatalf(format string, args ...interface{}) {

cmd/hivechain/accounts.go
@@ -1,0 +1,99 @@
+package main
+
+import (
+	"crypto/ecdsa"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+var knownAccounts = []genAccount{
+	{
+		key:  mustParseKey("4552dbe6ca4699322b5d923d0c9bcdd24644f5db8bf89a085b67c6c49b8a1b91"),
+		addr: common.HexToAddress("0x7435ed30A8b4AEb0877CEf0c6E8cFFe834eb865f"),
+	},
+	{
+		key:  mustParseKey("f6a8f1603b8368f3ca373292b7310c53bec7b508aecacd442554ebc1c5d0c856"),
+		addr: common.HexToAddress("0x84E75c28348fB86AceA1A93a39426d7D60f4CC46"),
+	},
+	{
+		key:  mustParseKey("6e1e16a9c15641c73bf6e237f9293ab1d4e7c12b9adf83cfc94bcf969670f72d"),
+		addr: common.HexToAddress("0x4ddE844b71bcdf95512Fb4Dc94e84FB67b512eD8"),
+	},
+	{
+		key:  mustParseKey("fc39d1c9ddbba176d806ebb42d7460189fe56ca163ad3eb6143bfc6beb6f6f72"),
+		addr: common.HexToAddress("0xd803681E487E6AC18053aFc5a6cD813c86Ec3E4D"),
+	},
+	{
+		key:  mustParseKey("a88293fefc623644969e2ce6919fb0dbd0fd64f640293b4bf7e1a81c97e7fc7f"),
+		addr: common.HexToAddress("0x4a0f1452281bCec5bd90c3dce6162a5995bfe9df"),
+	},
+	{
+		key:  mustParseKey("457075f6822ac29481154792f65c5f1ec335b4fea9ca20f3fea8fa1d78a12c68"),
+		addr: common.HexToAddress("0x14e46043e63D0E3cdcf2530519f4cFAf35058Cb2"),
+	},
+	{
+		key:  mustParseKey("9ee3fd550664b246ad7cdba07162dd25530a3b1d51476dd1d85bbc29f0592684"),
+		addr: common.HexToAddress("0xE7d13f7Aa2A838D24c59b40186a0aCa1e21CffCC"),
+	},
+	{
+		key:  mustParseKey("865898edcf43206d138c93f1bbd86311f4657b057658558888aa5ac4309626a6"),
+		addr: common.HexToAddress("0x16c57eDF7Fa9D9525378B0b81Bf8A3cEd0620C1c"),
+	},
+	{
+		key:  mustParseKey("19168cd7767604b3d19b99dc3da1302b9ccb6ee9ad61660859e07acd4a2625dd"),
+		addr: common.HexToAddress("0x2D389075BE5be9F2246Ad654cE152cF05990b209"),
+	},
+	{
+		key:  mustParseKey("ee7f7875d826d7443ccc5c174e38b2c436095018774248a8074ee92d8914dcdb"),
+		addr: common.HexToAddress("0x1F4924B14F34e24159387C0A4CdBaa32f3DDb0cF"),
+	},
+	{
+		key:  mustParseKey("bfcd0e032489319f4e5ca03e643b2025db624be6cf99cbfed90c4502e3754850"),
+		addr: common.HexToAddress("0x0c2c51a0990AeE1d73C1228de158688341557508"),
+	},
+	{
+		key:  mustParseKey("41be4e00aac79f7ffbb3455053ec05e971645440d594c047cdcc56a3c7458bd6"),
+		addr: common.HexToAddress("0x5f552da00dFB4d3749D9e62dCeE3c918855A86A0"),
+	},
+	{
+		key:  mustParseKey("71aa7d299c7607dabfc3d0e5213d612b5e4a97455b596c2f642daac43fa5eeaa"),
+		addr: common.HexToAddress("0x3aE75c08b4c907EB63a8960c45B86E1e9ab6123c"),
+	},
+	{
+		key:  mustParseKey("c825f31cd8792851e33a290b3d749e553983111fc1f36dfbbdb45f101973f6a9"),
+		addr: common.HexToAddress("0x654aa64f5FbEFb84c270eC74211B81cA8C44A72e"),
+	},
+	{
+		key:  mustParseKey("8d0faa04ae0f9bc3cd4c890aa025d5f40916f4729538b19471c0beefe11d9e19"),
+		addr: common.HexToAddress("0x717f8AA2b982BeE0e29f573D31Df288663e1Ce16"),
+	},
+	{
+		key:  mustParseKey("47f666f20e2175606355acec0ea1b37870c15e5797e962340da7ad7972a537e8"),
+		addr: common.HexToAddress("0x4340Ee1b812ACB40a1eb561C019c327b243b92Df"),
+	},
+	{
+		key:  mustParseKey("8d56bcbcf2c1b7109e1396a28d7a0234e33544ade74ea32c460ce4a443b239b1"),
+		addr: common.HexToAddress("0xC7B99a164Efd027a93f147376Cc7DA7C67c6bbE0"),
+	},
+	{
+		key:  mustParseKey("34391cbbf06956bb506f45ec179cdd84df526aa364e27bbde65db9c15d866d00"),
+		addr: common.HexToAddress("0x83C7e323d189f18725ac510004fdC2941F8C4A78"),
+	},
+	{
+		key:  mustParseKey("25e6ce8611cefb5cd338aeaa9292ed2139714668d123a4fb156cabb42051b5b7"),
+		addr: common.HexToAddress("0x1F5BDe34B4afC686f136c7a3CB6EC376F7357759"),
+	},
+	{
+		key:  mustParseKey("14cdde09d1640eb8c3cda063891b0453073f57719583381ff78811efa6d4199f"),
+		addr: common.HexToAddress("0xedA8645bA6948855E3B3cD596bbB07596d59c603"),
+	},
+}
+
+func mustParseKey(s string) *ecdsa.PrivateKey {
+	key, err := crypto.HexToECDSA(s)
+	if err != nil {
+		panic(err)
+	}
+	return key
+}

commit 1c5b5b8b5e568e4450cbff6a6d6d99ea49fab6c7

cmd/hivechain: fix -outputs all expansion
The expanded output list was unconditionally overwritten by
splitAndTrim("all") right below, so -outputs all failed with an
unknown output name ever since the flag was introduced in #909.

cmd/hivechain/main.go
@@ -76,8 +76,9 @@ func generateCommand(args []string) {
 	if *outlist != "" {
 		if *outlist == "all" {
 			cfg.outputs = outputFunctionNames()
+		} else {
+			cfg.outputs = splitAndTrim(*outlist)
 		}
-		cfg.outputs = splitAndTrim(*outlist)
 	}
 	for _, name := range splitAndTrim(*disabled) {
 		if _, ok := modRegistry[name]; !ok {